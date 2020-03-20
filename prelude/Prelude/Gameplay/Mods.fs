68dfcf4c0212c1d9858cf24ef0f47131916c565e
Percyqaz <percyqaz@yahoo.co.uk>
2020-03-20 14:14:09 +0000

    more mod logic and stub color coding

--- prelude/Prelude/Gameplay/Mods.fs
+++ prelude/Prelude/Gameplay/Mods.fs
@@ -1,6 +1,7 @@
 ﻿module Prelude.Gameplay.Mods
 
 open System.Collections.Generic
+open Prelude.Common
 open Prelude.Charts.Interlude
 open Prelude.Gameplay.Score
 
@@ -10,9 +11,9 @@ type ModConfig = int
     Marker for status of mods.
         0 = This is a non-silly mod suitable for online upload, personal bests, leaderboards
         1 = This is a for-fun mod that may transform a chart in large ways or are otherwise not suitable for leaderboards e.g. randomiser
-        2 = Scores with this mod enabled should not be saved at all e.g. when testing/developing
+        2 = Scores with this mod enabled should not be saved at all e.g. when testing/developing or autoplay
 *)
-type ModStatus = Ranked = 0 | Unranked = 1 | Unstable = 2
+type ModStatus = Ranked = 0 | Unranked = 1 | Unstored = 2
 
 (*
     Mods (Modifiers) are a set of optional effects that can be enabled for a chart before playing it
@@ -26,6 +27,7 @@ type ModStatus = Ranked = 0 | Unranked = 1 | Unstable = 2
 type Mod
     (status : ModStatus, numStates : int, check : ModConfig -> Chart -> bool,
         apply : ModConfig -> Chart -> unit, apply_score : ModConfig -> Chart -> ScoreData -> unit) = 
+    member this.MaxState = numStates
     member this.Status = status
     //Looks at a chart and returns true if applying the modifier will do anything, otherwise false
     //Used to hide enabled mods that would have no effect e.g. a mod that removes hold notes on a chart with no hold notes
@@ -35,3 +37,29 @@ type Mod
     //Applies the modifier to the hit data of the chart - This hit data maps out what notes need to be hit/what notes have been hit
     //This typically is unused - examples uses are marking all notes as hit perfectly in Autoplay or marking LN releases as not needing to be hit
     member this.ApplyHitData = apply_score
+
+type ModState(mods : Dictionary<string, int>) =
+    static let modList = new Dictionary<string, Mod>();
+
+    member this.EnableMod id state = 
+        if modList.[id].MaxState >= state then
+            mods.[id] <- state
+        else failwith "tried to assign a state"
+    member this.DisableMod id = mods.Remove(id)
+    member this.IterApplicable chart =
+        seq {
+            for id in mods.Keys do
+                if modList.[id].CheckApplicable (mods.[id]) chart then yield modList.[id]
+            }
+
+    static member ModList = modList
+    static member RegisterMod id obj = modList.Add(id, obj)
+    static member GetModName id = localise ("mod."+id+".name")
+    static member GetModDesc id = localise ("mod."+id+".desc")
+
+let private auto _ _ hitData =
+    for (t, delta, hit) in hitData do
+        for i = 0 to (Array.length hit - 1) do
+            if hit.[i] = HitStatus.Not_Hit then hit.[i] <- HitStatus.Hit
+ModState.RegisterMod "auto"
+    (Mod(ModStatus.Unstored, 0, (fun _ _ -> true), (fun _ _ -> ()), auto))
---
+++ prelude/Prelude/Gameplay/NoteColors.fs
@@ -0,0 +1,29 @@
+﻿module Prelude.Gameplay.Colors
+
+open Prelude.Charts.Interlude
+
+//This is the final stage of preprocessing chart data before it is played by the user.
+//Colorings are an assignment of a color id for each note. These ids are then used by skins to display differences in textures
+//Some players may find certain coloring systems useful, for example having color coding depending on the musical beat a note is snapped to
+//It is also common just to have simple column color variation to make columns appear distinct
+
+let DDRValues = [|1; 2; 3; 4; 6; 8; 12; 16|]
+
+type ColorScheme = 
+    | Column
+    | Chord
+    | DDR
+    | Jackhammer
+    member this.ColorCount keycount =
+        match this with
+        | Column -> keycount
+        | Chord -> keycount
+        | DDR -> Array.length DDRValues
+        | Jackhammer -> Array.length DDRValues
+
+type ColorData = int array //always of size 10
+type ColorDataSets = ColorData array //color config per keymode. 0 stores "all keymode" data, 1 stores 3k, 2 stores 4k, etc
+
+let applyColorizer (scheme : ColorScheme) (colorData : ColorData) (chart : Chart) =
+    match scheme with
+    | _ -> ()
