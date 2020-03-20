2244eaeb0d6d54d25120d3ae26f7fd8a542b6801
Percyqaz <percyqaz@yahoo.co.uk>
2020-03-20 17:11:07 +0000

    bitmapping code for transforming column arrangements - can be extended to support more complicated relations easily - mirror is an example use - used this code for a chart project in prelude-test

--- prelude/Prelude-Test/Program.fs
+++ prelude/Prelude-Test/Program.fs
@@ -1,8 +1,10 @@
 ﻿open System
 open Prelude.Common;
+open Prelude.Charts.Interlude
+open Prelude.Charts.osu
 open Prelude.Charts.ChartConversions
+open Prelude.Charts.Filter
 open Prelude.Gameplay.Difficulty
-open Collatz
 
 (*
     Prelude features to tackle next:
@@ -17,11 +19,43 @@ open Collatz
 
 [<EntryPoint>]
 let main argv =
-    (*
     let chart =
-        @"C:\Users\Blake\AppData\Local\osu!\Songs\911479 HO-KAGO TEA TIME - Utauyo!! MIRACLE (TV size) [no video]\HO-KAGO TEA TIME - Utauyo!! MIRACLE (TV size) (Kawawa) [BMS LV.11 wawather [Stream -Another-]].osu"
+        @"C:\Users\percy\AppData\Local\osu!\Songs\1122880 WJSN (Cosmic Girls) - Miracle\WJSN (Cosmic Girls) - Miracle (Percyqaz) [Uncut Ver.].osu"
         |> loadAndConvertFile |> List.head
-    let r = RatingReport(chart.Notes, 0.9, Layout.LeftOne, chart.Keys)
-    printf "%A, %A" r.Physical r.Technical*)
-    Collatz.main
+    let all = textMapping       "--1234--"
+    let luda = textMapping      "-1234---"
+    let eunseo = textMapping    "---1234-"
+    let chengXiao = textMapping "--12-34-"
+    let bona = textMapping      "---1234-"
+    let dayoung = textMapping   "----1234"
+    let yeonjung = textMapping  "-1234---"
+    let seolA = textMapping     "12-34---"
+    let dawon = textMapping     "--1234--"
+    let soobin = textMapping    "-12-34--"
+    let exy = textMapping       "1234----"
+    let yeoreum = textMapping   "---1234-"
+    let xuanYi = textMapping    "--12-34-"
+    let meiQi = textMapping     "--1234--"
+    let transitions = [
+        (0.0, all);
+        (20085.0, luda); (28375.0, eunseo); (32875.0, chengXiao);
+        (36375.0, bona); (44375.0, dayoung);
+        (52875.0, yeonjung); (62625.0, seolA);
+        (69625.0, dawon); (71875.0, soobin); (73625.0, dawon); (76125.0, soobin); (78625.0, yeonjung); 
+        (86125.0, exy);
+        (104125.0, luda); (108125.0, yeoreum); (112125.0, xuanYi); 
+        (120375.0, meiQi); (128125.0, dayoung); 
+        (136875.0, yeonjung); (146625.0, seolA); 
+        (154125.0, soobin); (162625.0, dawon);
+        (170875.0, dayoung); (180625.0, seolA);
+        (187625.0, dawon); (189875.0, soobin); (191625.0, dawon); (194125.0, soobin); (196625.0, yeonjung);
+        (204085.0, all);
+        (220085.0, all)
+        ]
+    Seq.zip transitions (List.tail transitions) |> List.ofSeq
+    |> List.map (fun ((t1, m), (t2, _)) -> applyMappingBetween m t1 t2 chart)
+    |> ignore
+    Chart(8, chart.Header, chart.Notes, chart.BPM, chart.SV)
+    |> convert_interlude_osu
+    |> saveBeatmapFile @"C:\Users\percy\AppData\Local\osu!\Songs\1122880 WJSN (Cosmic Girls) - Miracle\WJSN (Cosmic Girls) - Miracle (Percyqaz) [Dumb Files Ver.].osu"
     0 
