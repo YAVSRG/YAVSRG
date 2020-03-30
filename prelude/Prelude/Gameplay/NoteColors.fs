ac078630715d0635ed9be580e4e3f2f501bdbcac
Percyqaz <percyqaz@yahoo.co.uk>
2020-03-30 13:48:26 +0100

    colorizer code

--- prelude/Prelude/Gameplay/NoteColors.fs
+++ prelude/Prelude/Gameplay/NoteColors.fs
@@ -1,5 +1,6 @@
 ﻿module Prelude.Gameplay.Colors
 
+open System
 open Prelude.Charts.Interlude
 
 //This is the final stage of preprocessing chart data before it is played by the user.
@@ -23,13 +24,38 @@ type ColorScheme =
 
 type ColorData = byte array //always of size 10
 type ColorDataSets = ColorData array //color config per keymode. 0 stores "all keymode" data, 1 stores 3k, 2 stores 4k, etc
+type Colorizer<'state> = 'state -> TimeDataItem<NoteRow> -> ('state * ColorData)
 
+let colorize (chart : Chart) (initialState, col : Colorizer<'t>) =
+    (Seq.fold (fun (s, data : (TimeDataItem<NoteRow * ColorData>) list) (time, nr) -> ( let (ns, d) = col s (time, nr) in (ns, (time, (nr, d)) :: data) ))
+        (initialState, []) chart.Notes.Enumerate
+        |> snd)
+    |> Seq.rev
+    |> ResizeArray<TimeDataItem<NoteRow * ColorData>>
+    |> TimeData<NoteRow * ColorData>
 
+let private roughlyDivisible (a : float) (b : float) = Math.Abs(a - b * Math.Round(a / b)) < 3.0
+
+let private ddr_func delta (msPerBeat : float) : int =
+    List.tryFind ((fun i -> DDRValues.[i]) >> fun n -> roughlyDivisible delta (msPerBeat / (float n))) [0..7]
+    |> Option.defaultValue DDRValues.Length
 
 let applyColorizer (scheme : ColorScheme) (colorData : ColorData) (chart : Chart) =
     let ci i = colorData.[i]
     match scheme with
     | Column ->
-        for (_, nr) in chart.Notes.Enumerate do
-            ()
-    | _ -> ()
+        ((), fun _ (_, nr) ->
+            ((), [|for i in 0..(chart.Keys-1) -> ci i|]))
+            |> colorize chart
+    | Chord ->
+        ((), fun _ (_, nr) ->
+            ((), Array.create chart.Keys ((nr |> noteData NoteType.NORMAL |> countBits) + (nr |> noteData NoteType.HOLDHEAD |> countBits) |> ci)))
+            |> colorize chart
+    | DDR ->
+        (chart, fun c (time, nr) ->
+            let (ptime,(_, msPerBeat)) = chart.BPM.GetPointAt(time) in (chart, Array.create chart.Keys ((ddr_func (time - ptime) msPerBeat) |> ci)))
+            |> colorize chart
+    | _ ->
+        ((), fun _ _ ->
+            ((), Array.zeroCreate(chart.Keys)))
+            |> colorize chart
