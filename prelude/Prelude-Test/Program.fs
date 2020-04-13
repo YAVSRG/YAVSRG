5375e768ec441930036d164da3a481c836cb037a
Percyqaz <percyqaz@yahoo.co.uk>
2020-04-13 17:22:00 +0100

    added json support for union types, with special case for option types

--- prelude/Prelude-Test/Program.fs
+++ prelude/Prelude-Test/Program.fs
@@ -15,14 +15,26 @@ open Prelude.Data.ChartManager
         SV Filters
 *)
 
+type Trichotomy = A | B of Trichotomy * int | C of int
+
+type testRec = {
+    foo: Trichotomy option
+    bar: Trichotomy
+}
+
 [<EntryPoint>]
 let main argv =
     Console.BufferHeight <- 32766
-    printf "%A" (Prelude.Json.JsonHelper.load "{}": Prelude.Charts.Interlude.ChartHeader)
-    printf "%A" (Prelude.Json.JsonHelper.load "{}": Prelude.Data.Profiles.Profile).Name
-    Logging.Subscribe(fun (_, _, s) -> if s.Length > 0 then printfn "    %s\n" s)
+    let x:testRec =
+        (Prelude.Json.JsonHelper.save {foo = Some (B (A, 5)); bar = C 8})
+        |> fun x ->
+            printfn "%A" x
+            x
+        |> Prelude.Json.JsonHelper.load
+    printf "%A" x
+    (*Logging.Subscribe(fun (_, _, s) -> if s.Length > 0 then printfn "    %s\n" s)
     let c = Cache()
     @"C:\Users\percy\AppData\Local\osu!\Songs\588322 Snails - Funk With Me (ft Big Gigantic)"
     |> fun p -> TaskManager.AddTask("A", (c.ConvertSongFolder p "osu!"), (fun b -> TaskManager.AddTask("B", (c.RebuildCache), ignore, true)), true)
-    TaskManager.Wait
+    TaskManager.Wait*)
     0
--- prelude/Prelude/Json.fs
+++ prelude/Prelude/Json.fs
@@ -1,5 +1,6 @@
 ﻿namespace Prelude
 
+open System
 open System.IO
 open System.Linq
 open Microsoft.FSharp.Reflection
@@ -22,8 +23,91 @@ module Json =
             let items = array.Select(fun a i -> a.ToObject(argTypes.[i], serializer)).ToArray()
             FSharpValue.MakeTuple(items, t)
 
+    type UnionConverter() =
+        inherit JsonConverter()
+
+        override this.CanConvert t = FSharpType.IsUnion t
+
+        override this.WriteJson(writer, value, serializer) =
+            let t = value.GetType()
+            let caseInfo, fieldValues = FSharpValue.GetUnionFields(value, t)
+            writer.WriteStartObject()
+            writer.WritePropertyName(caseInfo.Name)
+            let value =
+                match fieldValues.Length with
+                | 0 -> null
+                | 1 -> fieldValues.[0]
+                | _ -> fieldValues :> obj
+            serializer.Serialize(writer, value)
+            writer.WriteEndObject()
+
+        override this.ReadJson(reader, t, existingValue, serializer) =
+            reader.Read() |> ignore
+            if reader.TokenType <> JsonToken.PropertyName then failwith "no property name found for union"
+            let name = reader.Value :?> string
+            let case, fields =
+                match FSharpType.GetUnionCases(t) |> Array.filter (fun case -> case.Name = name) with
+                | [|case|] -> case, case.GetFields()
+                | _ -> failwith "could not deserialise unknown union case"
+            printfn "matched case: %s with %i fields" case.Name fields.Length
+            reader.Read() |> ignore
+            let result =
+                match fields.Length with
+                | 0 ->
+                    //there should be a null token under the cursor
+                    FSharpValue.MakeUnion(case, [||])
+                | 1 ->
+                    printfn "reading type: %A" fields.[0].PropertyType
+                    let res = [| serializer.Deserialize(reader, fields.[0].PropertyType) |]
+                    printfn "read field: %A" res
+                    FSharpValue.MakeUnion(case, res)
+                | _ ->
+                    let tupleType =
+                        fields
+                        |> Seq.map (fun f -> f.PropertyType)
+                        |> Seq.toArray
+                        |> FSharpType.MakeTupleType
+                    let tuple = serializer.Deserialize(reader, tupleType)
+                    FSharpValue.MakeUnion(case, FSharpValue.GetTupleFields(tuple))
+            reader.Read() |> ignore
+            if reader.TokenType <> JsonToken.EndObject then failwith "end of object"
+            result
+
+    type OptionConverter() =
+        inherit JsonConverter()
+
+        override this.CanConvert t = t.IsGenericType && typedefof<option<_>>.Equals(t.GetGenericTypeDefinition())
+
+        override this.WriteJson(writer, value, serializer) =
+            let value =
+                match value = null with
+                | true -> null
+                | false ->
+                    let _, fields = FSharpValue.GetUnionFields(value, value.GetType())
+                    fields.[0]
+            serializer.Serialize(writer, value)
+
+        override this.ReadJson(reader, t, existingValue, serializer) =
+            let innerType = t.GetGenericArguments().[0]
+
+            let innerType = 
+              if innerType.IsValueType then
+                typedefof<Nullable<_>>.MakeGenericType([| innerType |])
+              else
+                innerType
+
+            let value = serializer.Deserialize(reader, innerType)
+            let cases = FSharpType.GetUnionCases t
+
+            if value = null then
+              FSharpValue.MakeUnion(cases.[0], [||])
+            else
+              FSharpValue.MakeUnion(cases.[1], [|value|])
+
     let addConverters (settings : JsonSerializerSettings) : JsonSerializerSettings = 
         settings.Converters.Add(TupleConverter())
+        settings.Converters.Add(OptionConverter())
+        settings.Converters.Add(UnionConverter())
         settings
 
     module JsonHelper = 
