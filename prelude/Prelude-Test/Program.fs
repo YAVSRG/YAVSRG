b2a4ca0961feed1ae9981f8c617a2a4684024a58
Percyqaz <percyqaz@yahoo.co.uk>
2020-04-21 02:24:20 +0100

    some theme support - most functions will be done inside prelude so that noteskin/osu skin conversion can be built on top of it

--- prelude/Prelude-Test/Program.fs
+++ prelude/Prelude-Test/Program.fs
@@ -2,7 +2,6 @@
 open System.Collections.Generic
 open Prelude.Common
 open Prelude.Json
-open FParsec
 open Prelude.Data.ChartManager
 
 (*
@@ -17,9 +16,4 @@ open Prelude.Data.ChartManager
 [<EntryPoint>]
 let main argv =
     Console.BufferHeight <- 32766
-    (*Logging.Subscribe(fun (_, _, s) -> if s.Length > 0 then printfn "    %s\n" s)
-    let c = Cache()
-    @"C:\Users\percy\AppData\Local\osu!\Songs\588322 Snails - Funk With Me (ft Big Gigantic)"
-    |> fun p -> TaskManager.AddTask("A", (c.ConvertSongFolder p "osu!"), (fun b -> TaskManager.AddTask("B", (c.RebuildCache), ignore, true)), true)
-    TaskManager.Wait*)
     0
