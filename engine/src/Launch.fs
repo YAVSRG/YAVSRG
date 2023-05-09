125db89e8464981547fef3e951734fda8ef35256
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-05-09 12:38:15 +0100

    🧱 Refactor logging system

--- engine/src/Launch.fs
+++ engine/src/Launch.fs
@@ -1,7 +1,6 @@
 ﻿namespace Percyqaz.Flux
 
 open System
-open System.IO
 open Percyqaz.Common
 open Percyqaz.Flux.Windowing
 open Percyqaz.Flux.UI
@@ -9,13 +8,6 @@ open Percyqaz.Flux.UI
 module Launch =
 
     let entryPoint (config: Config, name: string, root: Root) =
-        use logfile = File.Open("log.txt", FileMode.Append)
-        use sw = new StreamWriter(logfile)
-        Logging.Subscribe
-            ( fun (level, main, details) ->
-                if details = "" then sprintf "[%A] %s" level main else sprintf "[%A] %s\n%s" level main details
-                |> sw.WriteLine )
-    
         Logging.Info(sprintf "Launching %s: %O" name (DateTime.Now.ToString()))
         let window =
             try
@@ -30,4 +22,3 @@ module Launch =
             with err -> Logging.Critical("Fatal error in window thread", err); crashed <- true
             window.Close()
             if crashed then ignore(Console.ReadLine())
-        Logging.Wait()
