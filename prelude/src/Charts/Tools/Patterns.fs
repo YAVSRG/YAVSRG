cb084a40399568612f15b57be763db6fd7586da9
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-07-02 19:03:47 +0100

    🚧 Adjustments to pattern analysis system

--- prelude/src/Charts/Tools/Patterns.fs
+++ prelude/src/Charts/Tools/Patterns.fs
@@ -48,70 +48,46 @@ type Direction =
     | Outwards
     | Inwards
 
+type [<Measure>] rate
+type ScaledTime = float32<ms/rate>
+
 type RowInfo =
     {
         Notes: int
         Jacks: int
         Direction: Direction
         Roll: bool
-        Time: Time
+        Time: float32<ms/rate>
         MsPerBeat: float32<ms/beat>
+        Density: float32
     }
 
 module Analysis =
 
-    let run (chart: Chart) : RowInfo list =
-        
-        let { Time = t; Data = row } = (TimeArray.first chart.Notes).Value
-        let mutable previous_row =
-            seq {0 .. chart.Keys - 1}
-            |> Seq.filter (fun x -> row.[x] = NoteType.NORMAL || row.[x] = NoteType.HOLDHEAD)
-            |> Array.ofSeq
-        let mutable previous_time = t
+    let private DENSITY_SENTITIVITY = -10f
 
+    let private density_step(time: Time) (d: float32) =
+        let seconds = time / 1000f<ms>
+        d * System.MathF.Exp(DENSITY_SENTITIVITY * seconds)
+
+    let private density_hit(time: Time) (d: float32) =
+        let seconds = time / 1000f<ms>
+        density_step time d + (1.0f - System.MathF.Exp(DENSITY_SENTITIVITY * seconds)) / seconds
+
+    let density_data (rate: float32) (chart: Chart) =
+        let column_densities = Array.zeroCreate chart.Keys
+        let column_sinces = Array.create chart.Keys -Time.infinity
         seq {
-            for { Time = t; Data = row } in (chart.Notes |> Seq.skip 1) do
-                
-                let current_row = 
-                    seq {0 .. chart.Keys - 1}
-                    |> Seq.filter (fun x -> row.[x] = NoteType.NORMAL || row.[x] = NoteType.HOLDHEAD)
-                    |> Array.ofSeq
+            for { Time = t; Data = row } in chart.Notes do
+                for k = 0 to chart.Keys - 1 do
+                    if row.[k] = NoteType.NORMAL || row.[k] = NoteType.HOLDHEAD then 
+                        column_densities.[k] <- density_hit ((t - column_sinces.[k]) / rate) column_densities.[k]
+                        column_sinces.[k] <- t
+                yield Array.max column_densities
+        }
+        |> Array.ofSeq
 
-                if current_row.Length > 0 then
-
-                    let pmin = Array.min previous_row
-                    let pmax = Array.max previous_row
-                    let cmin = Array.min current_row
-                    let cmax = Array.max current_row
-
-                    yield {
-                        Notes = current_row.Length
-                        Jacks = current_row.Length - (Array.except previous_row current_row).Length
-                        Direction =
-                            let lo = cmin - pmin
-                            let hi = cmax - pmax
-
-                            if lo > 0 then
-                                if hi > 0 then Direction.Right
-                                else Direction.Inwards
-                            elif lo < 0 then
-                                if hi < 0 then Direction.Left
-                                else Direction.Outwards
-                            else
-                                if hi < 0 then Direction.Inwards
-                                elif hi > 0 then Direction.Outwards
-                                else Direction.None
-                        Roll = pmin > cmax || pmax < cmin
-                        Time = t
-                        MsPerBeat = (t - previous_time) * 4.0f</beat>
-                    }
-
-                    previous_row <- current_row
-                    previous_time <- t
-
-        } |> List.ofSeq
-
-    let density (samples: int) (chart: Chart) : int array * int array =
+    let nps_cps (samples: int) (chart: Chart) : int array * int array =
         let mutable i = 0
         let mutable notes = 0
         let mutable rows = 0
@@ -144,6 +120,61 @@ module Analysis =
                 rowcounts.[samples - 1] <- rows - last_sample_rows
 
         notecounts, rowcounts
+
+    let run (rate: float32) (chart: Chart) : RowInfo list =
+    
+        let { Time = firstNote; Data = row } = (TimeArray.first chart.Notes).Value
+        let density = density_data rate chart
+        let mutable previous_row =
+            seq {0 .. chart.Keys - 1}
+            |> Seq.filter (fun x -> row.[x] = NoteType.NORMAL || row.[x] = NoteType.HOLDHEAD)
+            |> Array.ofSeq
+        let mutable previous_time = firstNote
+
+        let mutable index = 0
+
+        seq {
+            for { Time = t; Data = row } in (chart.Notes |> Seq.skip 1) do
+                index <- index + 1
+                let current_row = 
+                    seq {0 .. chart.Keys - 1}
+                    |> Seq.filter (fun x -> row.[x] = NoteType.NORMAL || row.[x] = NoteType.HOLDHEAD)
+                    |> Array.ofSeq
+
+                if current_row.Length > 0 then
+
+                    let pmin = Array.min previous_row
+                    let pmax = Array.max previous_row
+                    let cmin = Array.min current_row
+                    let cmax = Array.max current_row
+
+                    yield {
+                        Notes = current_row.Length
+                        Jacks = current_row.Length - (Array.except previous_row current_row).Length
+                        Direction =
+                            let lo = cmin - pmin
+                            let hi = cmax - pmax
+
+                            if lo > 0 then
+                                if hi > 0 then Direction.Right
+                                else Direction.Inwards
+                            elif lo < 0 then
+                                if hi < 0 then Direction.Left
+                                else Direction.Outwards
+                            else
+                                if hi < 0 then Direction.Inwards
+                                elif hi > 0 then Direction.Outwards
+                                else Direction.None
+                        Roll = pmin > cmax || pmax < cmin
+                        Time = (t - firstNote) / (rate * 1.0f<rate>)
+                        MsPerBeat = (t - previous_time) * 4.0f</beat> / rate
+                        Density = density.[index]
+                    }
+
+                    previous_row <- current_row
+                    previous_time <- t
+
+        } |> List.ofSeq
 
 type Pattern = RowInfo list -> bool
 
@@ -311,12 +342,14 @@ module Patterns =
                 :: _ when x > 1 && y > 1 && z > 1 -> true
             | _ -> false
 
-    let matches (patterns: IDictionary<string, Pattern>) (data: RowInfo list) : (string * (Time * float32<ms/beat>)) seq =
+    type PatternToken = { Time: ScaledTime; MsPerBeat: float32<ms/beat>; Density: float32 }
+
+    let matches (patterns: IDictionary<string, Pattern>) (data: RowInfo list) : (string * PatternToken) seq =
         let mutable data = data
         seq {
             while not data.IsEmpty do
                 for pattern_name in patterns.Keys do
-                    if patterns.[pattern_name] data then yield (pattern_name, (data.Head.Time, data.Head.MsPerBeat))
+                    if patterns.[pattern_name] data then yield (pattern_name, { Time = data.Head.Time; MsPerBeat = data.Head.MsPerBeat; Density = data.Head.Density })
                 data <- List.tail data
         }
 
@@ -377,20 +410,20 @@ module Patterns =
             "Jumpgluts", (Color.Magenta, 100, 200)
         ]
 
-    let analyse (chart: Chart) =
-        let data = Analysis.run chart
+    let analyse (rate: float32) (chart: Chart) =
+        let data = Analysis.run rate chart
         if chart.Keys = 4 then matches analysis_4k data
         elif chart.Keys = 7 then matches analysis_7k data
         else matches analysis_generic data
 
-    type PatternLocation = { Time: Time; Duration: Time; BPM: int }
+    type PatternLocation = { Time: ScaledTime; Duration: ScaledTime; BPM: int; AverageDensity: float32 }
 
     type private BPMCluster = { mutable MsPerBeat: float32<ms/beat>; mutable Size: int }
 
     let private BPM_CLUSTER_THRESHOLD = 5.0f<ms/beat>
 
-    let pattern_locations (rate: float32) (pattern_tokens: (string * (Time * float32<ms/beat>)) seq) : (string * PatternLocation) seq =
-        let PATTERN_DURATION = 600.0f<ms> * rate
+    let pattern_locations (pattern_tokens: (string * PatternToken) seq) : (string * PatternLocation) seq =
+        let PATTERN_DURATION = 600.0f<ms/rate>
 
         let groups =
             pattern_tokens
@@ -399,8 +432,7 @@ module Patterns =
             |> Array.map (fun (pattern, data) -> 
                     pattern, 
                     Seq.map snd data 
-                    |> Seq.map (fun (t, mspb) -> (t, mspb / rate))
-                    |> Seq.filter (fun (t, mspb) -> mspb <= (60000.0f<ms/minute> / 85.0f<beat/minute>))
+                    |> Seq.filter (fun token -> token.MsPerBeat <= (60000.0f<ms/minute> / 85.0f<beat/minute>))
                     |> Array.ofSeq
                 )
 
@@ -412,37 +444,41 @@ module Patterns =
                 c.Size <- c.Size + 1
             | None -> clusters.Add({ Size = 1; MsPerBeat = value })
         for _, data in groups do
-            for (_, value) in data do cluster value
+            for token in data do cluster token.MsPerBeat
 
         let patterns = ResizeArray<string * PatternLocation>()
 
         for pattern_name, data in groups do
             let mutable current_n = 0
             let mutable current_mspb = 0.0f<ms/beat>
-            let mutable current_start = 0.0f<ms>
-            let mutable current_end = 0.0f<ms>
+            let mutable current_density = 0.0f
+            let mutable current_start = 0.0f<ms/rate>
+            let mutable current_end = 0.0f<ms/rate>
 
             let finish() =
                 let mspb = current_mspb / float32 current_n
+                let density = current_density / float32 current_n
                 let clustered_mspb =
                     match clusters |> Seq.tryFind (fun c -> abs (c.MsPerBeat - mspb) < BPM_CLUSTER_THRESHOLD) with
                     | Some c -> c.MsPerBeat
                     | None -> mspb
                 let bpm = (60000.0f<ms/minute> / clustered_mspb |> float32 |> round |> int)
-                patterns.Add((pattern_name, { BPM = bpm; Time = current_start; Duration = (current_end - current_start) / rate }))
+                patterns.Add((pattern_name, { BPM = bpm; Time = current_start; Duration = current_end - current_start; AverageDensity = density }))
                 current_n <- 0
 
-            for (t, mspb) in data do
-                if current_n > 0 && (abs ((current_mspb / float32 current_n) - mspb) < BPM_CLUSTER_THRESHOLD) && t <= current_end then
+            for token in data do
+                if current_n > 0 && (abs ((current_mspb / float32 current_n) - token.MsPerBeat) < BPM_CLUSTER_THRESHOLD) && token.Time <= current_end then
                     current_n <- current_n + 1
-                    current_mspb <- current_mspb + mspb
-                    current_end <- t + PATTERN_DURATION
+                    current_mspb <- current_mspb + token.MsPerBeat
+                    current_density <- current_density + token.Density
+                    current_end <- token.Time + PATTERN_DURATION
                 else
-                    if current_n > 0 then current_end <- min current_end t; finish()
+                    if current_n > 0 then current_end <- min current_end token.Time; finish()
                     current_n <- 1
-                    current_mspb <- mspb
-                    current_start <- t
-                    current_end <- t + PATTERN_DURATION
+                    current_mspb <- token.MsPerBeat
+                    current_density <- token.Density
+                    current_start <- token.Time
+                    current_end <- token.Time + PATTERN_DURATION
 
             finish()
 
@@ -450,11 +486,12 @@ module Patterns =
 
     type PatternBreakdown = 
         { 
-            mutable TotalTime: Time
-            mutable Bursts: ResizeArray<Time * Time>
-            mutable Runs: ResizeArray<Time * Time>
-            mutable Sprints: ResizeArray<Time * Time>
-            mutable Marathons: ResizeArray<Time * Time>
+            mutable TotalTime: ScaledTime
+            mutable DensityTime: ScaledTime
+            mutable Bursts: ResizeArray<ScaledTime * ScaledTime>
+            mutable Runs: ResizeArray<ScaledTime * ScaledTime>
+            mutable Sprints: ResizeArray<ScaledTime * ScaledTime>
+            mutable Marathons: ResizeArray<ScaledTime * ScaledTime>
         }
 
     let pattern_breakdown (patterns: (string * PatternLocation) seq) =
@@ -462,12 +499,13 @@ module Patterns =
         let coverage = Dictionary<string * int, PatternBreakdown>()
         for (pattern, info) in patterns do
             let key = (pattern, info.BPM)
-            if not <| coverage.ContainsKey(key) then coverage.Add(key, { TotalTime = 0.0f<ms>; Bursts = ResizeArray<_>(); Runs = ResizeArray<_>(); Sprints = ResizeArray<_>(); Marathons = ResizeArray<_>() })
+            if not <| coverage.ContainsKey(key) then coverage.Add(key, { TotalTime = 0.0f<ms/rate>; DensityTime = 0.0f<ms/rate>; Bursts = ResizeArray<_>(); Runs = ResizeArray<_>(); Sprints = ResizeArray<_>(); Marathons = ResizeArray<_>() })
             coverage.[key].TotalTime <- coverage.[key].TotalTime + info.Duration
+            coverage.[key].DensityTime <- coverage.[key].TotalTime + info.Duration * info.AverageDensity
             match info.Duration with
-            | x when x < 2000.0f<ms> -> coverage.[key].Bursts.Add((info.Time, info.Duration))
-            | x when x < 5000.0f<ms> -> coverage.[key].Runs.Add((info.Time, info.Duration))
-            | x when x < 30000.0f<ms> -> coverage.[key].Sprints.Add((info.Time, info.Duration))
+            | x when x < 2000.0f<ms/rate> -> coverage.[key].Bursts.Add((info.Time, info.Duration))
+            | x when x < 5000.0f<ms/rate> -> coverage.[key].Runs.Add((info.Time, info.Duration))
+            | x when x < 30000.0f<ms/rate> -> coverage.[key].Sprints.Add((info.Time, info.Duration))
             | _ -> coverage.[key].Marathons.Add((info.Time, info.Duration))
 
         coverage
