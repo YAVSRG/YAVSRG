a574295ed3022b952427054ac932b3df0de6e9ad
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-07-10 14:32:01 +0100

    📓 Wiki site generator

--- generate_site.fsx
+++ generate_site.fsx
@@ -85,3 +85,33 @@ File.ReadAllText("./Interlude/docs/changelog.md")
 |> Array.map Markdown.Parse
 |> Array.map MarkdownToHtml.render_document
 |> build_mpage "./site/changelog.html" "Changelog"
+
+let title (filename: string) =
+    let clean = filename.Replace(".md", "").Replace("_", " ")
+    clean.[0].ToString().ToUpper() + clean.Substring(1)
+
+let wiki_folders = 
+    Directory.EnumerateDirectories("./Interlude/docs/wiki/")
+    |> Seq.map (fun path -> (Path.GetFileName path, Directory.EnumerateFiles path |> Seq.map Path.GetFileName))
+
+let wiki_section (item: string, items: seq<string>) =
+    sprintf "<hr/><div class=\"ml-3\"><h2>%s</h2><ul class=\"ml-8 list-disc\">%s</ul></div>"
+        item
+        (items |> Seq.map (fun s -> sprintf "<li class=\"\"><a href=\"%s\">%s</a></li>" (s.Replace(".md", ".html")) (title s)) |> String.concat "")
+
+let wiki_sidebar_content = wiki_folders |> Seq.map wiki_section |> String.concat ""
+
+let wiki_template = File.ReadAllText("./site_data/wiki.html")
+for f, items in wiki_folders do
+    for i in items do
+        let content = 
+            File.ReadAllText("./Interlude/docs/wiki/" + f + "/" + i)
+                .Split([|"::::"|], System.StringSplitOptions.TrimEntries)
+            |> Array.map (Markdown.Parse >> MarkdownToHtml.render_document)
+            |> Array.map (sprintf "<div class=\"frame text-20 container flex flex-col mx-auto p-4\">%s</div>")
+            |> String.concat ""
+        wiki_template
+            .Replace("{{title}}", sprintf "%s - Interlude Wiki" (title i))
+            .Replace("{{content}}", content)
+            .Replace("{{sidebar}}", wiki_sidebar_content)
+        |> fun t -> File.WriteAllText("./site/interlude/wiki/" + i.Replace(".md", ".html"), t)
