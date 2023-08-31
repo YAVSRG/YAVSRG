d7ff71ba8a4ac2bc856e2b84d7e6e02aa7121e4b
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-08-31 23:35:59 +0100

    📓 Update scripts

--- online/run_server_in_docker.sh
+++ online/run_server_in_docker.sh
@@ -2,4 +2,4 @@ docker compose up --build --detach
 #winpty docker exec --privileged -it redis sh -c 'echo "vm.overcommit_memory=1" >> /etc/sysctl.conf'
 #winpty docker exec --privileged -it redis sh -c 'sysctl vm.overcommit_memory=1'
 
-$SHELL
+#$SHELL
