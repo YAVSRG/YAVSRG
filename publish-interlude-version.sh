0d89fa6d54ab2504d06355ff3df1907b36d921ba
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-10-18 13:23:49 +0100

    📘 Update wiki with parity to old wiki

--- publish-interlude-version.sh
+++ publish-interlude-version.sh
@@ -1,3 +1,4 @@
+cd Interlude/docs && ./update_wiki_toc.sh && cd ../..
 cd Interlude/tools && dotnet build --configuration Debug && cd bin/Debug/net7.0 && clear && dotnet run --project ../../.. -- publish
 cd ../../../../..
 ./generate_site.sh
