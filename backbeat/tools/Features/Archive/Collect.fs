ea3a6ad02bfada9296c3a4bdc99386a5fc0a27c8
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-11-17 20:25:41 +0000

    📘 Snake case naming, formatting

--- backbeat/tools/Features/Archive/Collect.fs
+++ backbeat/tools/Features/Archive/Collect.fs
@@ -14,22 +14,28 @@ open Backbeat.Utils
 module Collect =
 
     // matches lowercase text that could have been transliterated directly from japanese kana
-    let romaji_regex = Text.RegularExpressions.Regex("^((ssh|cch|ss|kk|tt|pp|ch|sh|[kstnhfmyrwgzdbpj])?y?[aiuoe]|n|tsu|dzu|\s)*$")
+    let romaji_regex =
+        Text.RegularExpressions.Regex("^((ssh|cch|ss|kk|tt|pp|ch|sh|[kstnhfmyrwgzdbpj])?y?[aiuoe]|n|tsu|dzu|\s)*$")
 
     let simplify_string =
         let regex = Text.RegularExpressions.Regex("[^\sa-zA-Z0-9_]")
         fun (s: string) -> regex.Replace(s.ToLowerInvariant(), "").Trim().Replace(" ", "")
 
-    let add_song(song: Song) : SongId =
+    let add_song (song: Song) : SongId =
 
-        let existing_id = songs.Keys |> Seq.tryFind (fun other_song_id -> song = songs.[other_song_id])
+        let existing_id =
+            songs.Keys |> Seq.tryFind (fun other_song_id -> song = songs.[other_song_id])
 
         match existing_id with
         | None ->
-            let id = (simplify_string (List.head song.Artists)) + "/" + (simplify_string song.Title)
+            let id =
+                (simplify_string (List.head song.Artists)) + "/" + (simplify_string song.Title)
+
             let mutable i = 0
+
             while songs.ContainsKey(if i > 0 then id + "-" + i.ToString() else id) do
                 i <- i + 1
+
             let id = if i > 0 then id + "-" + i.ToString() else id
             songs.Add(id, song)
             Logging.Info(sprintf "+ Song: %s" id)
@@ -43,25 +49,22 @@ module Collect =
             Logging.Info(sprintf "Rejecting %s because it doesn't have a background image" chart.Header.Title)
         else
 
-        let audio_hash = 
+        let audio_hash =
             match chart.Header.AudioFile with
-            | Asset s -> s 
-            | _ -> 
-                (Cache.audio_path chart backbeat_cache).Value
-                |> Cache.compute_hash
-        
-        let bg_hash = 
-            match chart.Header.BackgroundFile with
-            | Asset s -> s 
-            | _ -> 
-                (Cache.background_path chart backbeat_cache).Value
-                |> Cache.compute_hash
+            | Asset s -> s
+            | _ -> (Cache.audio_path chart backbeat_cache).Value |> Cache.compute_hash
 
-        let create_entry(song_id) =
+        let bg_hash =
+            match chart.Header.BackgroundFile with
+            | Asset s -> s
+            | _ -> (Cache.background_path chart backbeat_cache).Value |> Cache.compute_hash
+
+        let create_entry (song_id) =
             let lastNote = chart.LastNote
+
             {
                 SongId = song_id
-                Creators = [chart.Header.Creator]
+                Creators = [ chart.Header.Creator ]
                 Keys = chart.Keys
                 DifficultyName = chart.Header.DiffName
                 Subtitle = chart.Header.Subtitle
@@ -69,16 +72,19 @@ module Collect =
                 Duration = lastNote - chart.FirstNote
                 Notecount =
                     let mutable count = 0
+
                     for row in chart.Notes do
                         for k = 0 to chart.Keys - 1 do
-                            if row.Data.[k] = NoteType.NORMAL || row.Data.[k] = NoteType.HOLDHEAD then count <- count + 1
+                            if row.Data.[k] = NoteType.NORMAL || row.Data.[k] = NoteType.HOLDHEAD then
+                                count <- count + 1
+
                     count
                 BPM = Interlude.minMaxBPM (List.ofSeq chart.BPM) lastNote
-                Sources = 
+                Sources =
                     match chart.Header.ChartSource with
-                    | Origin.Osu (-1, 0) -> []
-                    | Origin.Osu (set, id) -> [Osu {| BeatmapSetId = set; BeatmapId = id |}]
-                    | Origin.Stepmania (id) -> [Stepmania id]
+                    | Origin.Osu(-1, 0) -> []
+                    | Origin.Osu(set, id) -> [ Osu {| BeatmapSetId = set; BeatmapId = id |} ]
+                    | Origin.Stepmania(id) -> [ Stepmania id ]
                     | Origin.Unknown -> []
                     @ extraSources
                 LastUpdated = DateTime.UtcNow
@@ -86,101 +92,130 @@ module Collect =
                 BackgroundFile = bg_hash
                 AudioFile = audio_hash
             }
-        
+
         let hash = Chart.hash chart
 
         if charts.ContainsKey hash then
             let old_entry = charts.[hash]
             let new_entry = create_entry old_entry.SongId
-            if new_entry.Sources.IsEmpty then Logging.Info(sprintf "Rejecting %s because it doesn't have a source" chart.Header.Title) else
-            if new_entry.Notecount < 50 then Logging.Info(sprintf "Rejecting %s because it doesn't have enough notes" chart.Header.Title) else
 
-            if { new_entry with LastUpdated = old_entry.LastUpdated } <> old_entry then
-                charts.[hash] <- 
+            if new_entry.Sources.IsEmpty then
+                Logging.Info(sprintf "Rejecting %s because it doesn't have a source" chart.Header.Title)
+            else if new_entry.Notecount < 50 then
+                Logging.Info(sprintf "Rejecting %s because it doesn't have enough notes" chart.Header.Title)
+            else
+
+                if
                     { new_entry with
-                        Tags = List.distinct (new_entry.Tags @ old_entry.Tags)
-                        Sources = List.distinct (new_entry.Sources @ old_entry.Sources)
+                        LastUpdated = old_entry.LastUpdated
                     }
-                Logging.Info(sprintf "^ Chart: %s" hash)
-            
-            upload_chart chart new_entry
+                    <> old_entry
+                then
+                    charts.[hash] <-
+                        { new_entry with
+                            Tags = List.distinct (new_entry.Tags @ old_entry.Tags)
+                            Sources = List.distinct (new_entry.Sources @ old_entry.Sources)
+                        }
+
+                    Logging.Info(sprintf "^ Chart: %s" hash)
+
+                upload_chart chart new_entry
         else
 
         let song: Song =
             {
-                Artists = [chart.Header.Artist]
+                Artists = [ chart.Header.Artist ]
                 OtherArtists = []
                 Remixers = []
                 Title = chart.Header.Title
-                AlternativeTitles = match chart.Header.TitleNative with Some x -> [x] | None -> []
+                AlternativeTitles =
+                    match chart.Header.TitleNative with
+                    | Some x -> [ x ]
+                    | None -> []
                 Source = chart.Header.Source
                 Tags = chart.Header.Tags
             }
+
         let song_id = add_song song
 
         // todo: record transliterations of artists as alternatives
         //match chart.Header.ArtistNative with Some a -> add_artist_alias (chart.Header.Artist, a) | None -> ()
-        
+
         let chart_entry = create_entry song_id
-        
-        if chart_entry.Sources.IsEmpty then Logging.Info(sprintf "Rejecting %s because it doesn't have a source" chart.Header.Title) else 
-        if chart_entry.Notecount < 50 then Logging.Info(sprintf "Rejecting %s because it doesn't have enough notes" chart.Header.Title) else
 
-        charts.Add(hash, chart_entry)
-        Logging.Info(sprintf "+ Chart: %s" hash)
+        if chart_entry.Sources.IsEmpty then
+            Logging.Info(sprintf "Rejecting %s because it doesn't have a source" chart.Header.Title)
+        else if chart_entry.Notecount < 50 then
+            Logging.Info(sprintf "Rejecting %s because it doesn't have enough notes" chart.Header.Title)
+        else
 
-        upload_chart chart chart_entry
+            charts.Add(hash, chart_entry)
+            Logging.Info(sprintf "+ Chart: %s" hash)
+
+            upload_chart chart chart_entry
 
     let slurp_folder (extra_sources: ChartSource list) (folder: string) =
         Directory.EnumerateFiles(Path.Combine(backbeat_cache.RootPath, folder))
-        |> Seq.map Chart.fromFile
+        |> Seq.map Chart.from_file
         |> Seq.iter (Option.iter (slurp_chart extra_sources))
-        save()
+
+        save ()
 
     let cache_song_folder (config: ConversionOptions) (target: string) =
         Directory.EnumerateFiles target
-        |> Seq.collect
-            (
-                function
-                | ChartFile _ as file ->
-                    try
-                        let action = 
-                            { 
-                                Config = config
-                                Source = file
-                            }
-                        loadAndConvertFile action
-                    with err -> Logging.Error ("Failed to convert/cache file: " + file, err); []
-                | _ -> []
-            )
+        |> Seq.collect (
+            function
+            | ChartFile _ as file ->
+                try
+                    let action = { Config = config; Source = file }
+                    loadAndConvertFile action
+                with err ->
+                    Logging.Error("Failed to convert/cache file: " + file, err)
+                    []
+            | _ -> []
+        )
         |> List.ofSeq
         |> fun charts -> Cache.add_new config.PackName charts backbeat_cache
 
     let cache_pack_folder (sm_pack_id: int) (target: string) =
         let pack_name = sm_pack_id.ToString()
-        for songFolder in Directory.EnumerateDirectories target do 
-            cache_song_folder 
-                { ConversionOptions.Default with StepmaniaPackId = Some sm_pack_id; MoveAssets = false; PackName = pack_name }
+
+        for songFolder in Directory.EnumerateDirectories target do
+            cache_song_folder
+                { ConversionOptions.Default with
+                    StepmaniaPackId = Some sm_pack_id
+                    MoveAssets = false
+                    PackName = pack_name
+                }
                 songFolder
+
         Cache.save backbeat_cache
         slurp_folder [] pack_name
 
     let cache_folder_of_oszs (community_pack_id: int) (target: string) =
         let pack_name = "c-" + community_pack_id.ToString()
-        for osz in Directory.EnumerateFiles target |> Seq.filter (fun f -> Path.GetExtension(f).ToLower() = ".osz") do
+
+        for osz in
+            Directory.EnumerateFiles target
+            |> Seq.filter (fun f -> Path.GetExtension(f).ToLower() = ".osz") do
             let extracted_path = Path.GetFileNameWithoutExtension osz
+
             if not (Directory.Exists extracted_path) then
                 ZipFile.ExtractToDirectory(osz, Path.GetFileNameWithoutExtension osz)
+
             cache_song_folder
-                { ConversionOptions.Default with MoveAssets = false; PackName = pack_name }
+                { ConversionOptions.Default with
+                    MoveAssets = false
+                    PackName = pack_name
+                }
                 extracted_path
+
         Cache.save backbeat_cache
-        slurp_folder [ChartSource.CommunityPack {| PackId = community_pack_id |}] pack_name
+        slurp_folder [ ChartSource.CommunityPack {| PackId = community_pack_id |} ] pack_name
 
-    let slurp_osu () =
-        slurp_folder [] "osu!"
+    let slurp_osu () = slurp_folder [] "osu!"
 
-    let rec try_download_mirrors(mirrors, filepath) : Async<bool> =
+    let rec try_download_mirrors (mirrors, filepath) : Async<bool> =
         async {
             match mirrors with
             | [] -> return false
@@ -188,24 +223,30 @@ module Collect =
                 if not (File.Exists filepath) then
                     Logging.Info(sprintf "Downloading from %s ..." url)
                     let! result = Prelude.Data.WebServices.download_file.RequestAsync(url, filepath, ignore)
-                    if result then return true 
-                    else 
+
+                    if result then
+                        return true
+                    else
                         Logging.Error(sprintf "Download from %s failed." url)
                         return! try_download_mirrors (xs, filepath)
-                else 
+                else
                     Logging.Info(sprintf "Download from %s already on disk ..." url)
                     return true
         }
 
-    let download_pack(id: StepmaniaPackId) =
+    let download_pack (id: StepmaniaPackId) =
         async {
             let mirrors = packs.Stepmania.[id].Mirrors |> List.map Archive.DownloadUrl.unpickle
             let zip = Path.Combine(ARCHIVE_PATH, "tmp", string id + ".zip")
             let folder = Path.Combine(ARCHIVE_PATH, "tmp", string id)
-            match! try_download_mirrors(mirrors, zip) with
+
+            match! try_download_mirrors (mirrors, zip) with
             | true ->
-                if Directory.Exists folder then Logging.Info (sprintf "Looks like this pack was already extracted to %s" folder)
-                else ZipFile.ExtractToDirectory(zip, folder)
+                if Directory.Exists folder then
+                    Logging.Info(sprintf "Looks like this pack was already extracted to %s" folder)
+                else
+                    ZipFile.ExtractToDirectory(zip, folder)
+
                 let pack_folder = (Directory.GetDirectories folder)[0]
                 cache_pack_folder id pack_folder
                 return true
@@ -213,53 +254,71 @@ module Collect =
                 Logging.Error "All mirrors failed."
                 return false
         }
-        
-    let download_community_pack(id: CommunityPackId) =
+
+    let download_community_pack (id: CommunityPackId) =
         async {
             let mirrors = packs.Community.[id].Mirrors |> List.map Archive.DownloadUrl.unpickle
             let zip = Path.Combine(ARCHIVE_PATH, "tmp", "c-" + string id + ".zip")
             let folder = Path.Combine(ARCHIVE_PATH, "tmp", "c-" + string id)
-            match! try_download_mirrors(mirrors, zip) with
+
+            match! try_download_mirrors (mirrors, zip) with
             | true ->
-                if Directory.Exists folder then Logging.Info (sprintf "Looks like this pack was already extracted to %s" folder)
-                else ZipFile.ExtractToDirectory(zip, folder)
+                if Directory.Exists folder then
+                    Logging.Info(sprintf "Looks like this pack was already extracted to %s" folder)
+                else
+                    ZipFile.ExtractToDirectory(zip, folder)
+
                 for f in Directory.GetDirectories folder do
                     cache_folder_of_oszs id f
+
                 return true
             | false ->
                 Logging.Error "All mirrors failed."
                 return false
         }
 
-    let download_osu_set(set_id: int) =
+    let download_osu_set (set_id: int) =
         async {
             let zip = Path.Combine(ARCHIVE_PATH, "tmp", "osu-" + string set_id + ".osz")
             let folder = Path.Combine(ARCHIVE_PATH, "tmp", "osu-" + string set_id)
-            match! try_download_mirrors([sprintf "https://api.chimu.moe/v1/download/%i?n=1" set_id], zip) with
+
+            match! try_download_mirrors ([ sprintf "https://api.chimu.moe/v1/download/%i?n=1" set_id ], zip) with
             | true ->
-                if Directory.Exists folder then Logging.Info (sprintf "Looks like this pack was already extracted to %s" folder)
-                else ZipFile.ExtractToDirectory(zip, folder)
-                cache_song_folder 
-                    { ConversionOptions.Default with MoveAssets = false; PackName = "osu!" }
+                if Directory.Exists folder then
+                    Logging.Info(sprintf "Looks like this pack was already extracted to %s" folder)
+                else
+                    ZipFile.ExtractToDirectory(zip, folder)
+
+                cache_song_folder
+                    { ConversionOptions.Default with
+                        MoveAssets = false
+                        PackName = "osu!"
+                    }
                     folder
+
                 Cache.save backbeat_cache
                 return true
-            | false -> 
+            | false ->
                 Logging.Error "Download failed."
                 return false
         }
 
     let slurp_sm (name: string) =
         let name = name.ToLower().Trim()
-        match packs.Stepmania |> Seq.tryFind(fun x -> x.Value.Title.ToLower() = name) with
+
+        match packs.Stepmania |> Seq.tryFind (fun x -> x.Value.Title.ToLower() = name) with
         | Some kv -> kv.Key |> download_pack |> Async.RunSynchronously |> ignore
         | None -> Logging.Error "Not found"
 
     let slurp_community (id: int) =
-        if packs.Community.ContainsKey id then 
+        if packs.Community.ContainsKey id then
             download_community_pack id |> Async.RunSynchronously |> ignore
-        else Logging.Error "Not found"
+        else
+            Logging.Error "Not found"
 
     let search_sm (name: string) =
         let name = name.ToLower().Trim()
-        for kvp in packs.Stepmania do if kvp.Value.Title.ToLower().Contains(name) then printfn "%s" kvp.Value.Title
+
+        for kvp in packs.Stepmania do
+            if kvp.Value.Title.ToLower().Contains(name) then
+                printfn "%s" kvp.Value.Title
--- backbeat/tools/Features/Archive/Upload.fs
+++ backbeat/tools/Features/Archive/Upload.fs
@@ -13,94 +13,137 @@ open Bytewizer.Backblaze.Models
 [<AutoOpen>]
 module Upload =
 
-    let client = 
-        let c = new BackblazeClient() in c.Connect(config.S3ApiKeyID, config.S3ApiKey) |> ignore; c
+    let client =
+        let c = new BackblazeClient() in
+        c.Connect(config.S3ApiKeyID, config.S3ApiKey) |> ignore
+        c
 
     let existing_files =
         let d = ConcurrentDictionary<string, bool>()
+
         match Queue.get "uploaded" with
         | [] ->
             Logging.Debug "No uploaded list, fetching from bucket"
+
             task {
                 // todo: stuff when file count goes over 10k
                 let request = ListFileNamesRequest("c44023fe407a500583900717", MaxFileCount = 10000)
                 let! files = client.Files.ListNamesAsync(request)
+
                 for f in files.Response.Files do
                     d.[f.FileName] <- true
                     Queue.append "uploaded" f.FileName
-            } |> Async.AwaitTask |> Async.RunSynchronously
-            Logging.Debug (sprintf "Found %i files in bucket" d.Count)
+            }
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+            Logging.Debug(sprintf "Found %i files in bucket" d.Count)
         | xs ->
             for file in xs do
                 d.[file] <- true
+
         d
 
     let add_existing_file (s: string) =
         existing_files.[s] <- true
         Queue.append "uploaded" s
-    
+
     let private upload_chart_internal (chart: Interlude.Chart) (chart_info: Chart) =
         match Interlude.Chart.check chart with
-        | Error msg ->
-            failwithf "Invalid chart to upload: %s" msg
+        | Error msg -> failwithf "Invalid chart to upload: %s" msg
         | Ok() ->
-        Async.Parallel [
-            task {
-                let hash = Interlude.Chart.hash chart
 
-                let exists = existing_files.ContainsKey hash
-                if not exists then
-                    use ms = new MemoryStream()
-                    use bw = new BinaryWriter(ms)
+        Async.Parallel
+            [
+                task {
+                    let hash = Interlude.Chart.hash chart
 
-                    Interlude.Chart.writeHeadless chart bw
-                    bw.Flush()
+                    let exists = existing_files.ContainsKey hash
 
-                    let! response = client.UploadAsync("c44023fe407a500583900717", hash, ms)
-                    response.EnsureSuccessStatusCode() |> ignore
-                    Logging.Info (sprintf "Uploaded %s" chart.Header.Title)
+                    if not exists then
+                        use ms = new MemoryStream()
+                        use bw = new BinaryWriter(ms)
 
-                    add_existing_file hash
-            } |> Async.AwaitTask
+                        Interlude.Chart.write_headless chart bw
+                        bw.Flush()
 
-            task {
-                let audio_hash = "assets/" + chart_info.AudioFile
+                        let! response = client.UploadAsync("c44023fe407a500583900717", hash, ms)
+                        response.EnsureSuccessStatusCode() |> ignore
+                        Logging.Info(sprintf "Uploaded %s" chart.Header.Title)
 
-                let exists = existing_files.ContainsKey audio_hash
-                if not exists then
-                    let! response = client.UploadAsync("c44023fe407a500583900717", audio_hash, File.OpenRead (Cache.audio_path chart backbeat_cache).Value)
-                    response.EnsureSuccessStatusCode() |> ignore
-                    Logging.Info(sprintf "Uploaded %s (%.1fMB)" audio_hash (float response.Response.ContentLength / 1000000.0))
+                        add_existing_file hash
+                }
+                |> Async.AwaitTask
 
-                    add_existing_file audio_hash
-            } |> Async.AwaitTask
-                
-            task {
-                let bg_hash = "assets/" + chart_info.BackgroundFile
-            
-                let exists = existing_files.ContainsKey bg_hash
-                if not exists then
-                    let! response = client.UploadAsync("c44023fe407a500583900717", bg_hash, File.OpenRead (Cache.background_path chart backbeat_cache).Value)
-                    response.EnsureSuccessStatusCode() |> ignore
-                    Logging.Info(sprintf "Uploaded %s (%.1fMB)" bg_hash (float response.Response.ContentLength / 1000000.0))
+                task {
+                    let audio_hash = "assets/" + chart_info.AudioFile
 
-                    add_existing_file bg_hash
-            } |> Async.AwaitTask
-        ] |> Async.Ignore
+                    let exists = existing_files.ContainsKey audio_hash
+
+                    if not exists then
+                        let! response =
+                            client.UploadAsync(
+                                "c44023fe407a500583900717",
+                                audio_hash,
+                                File.OpenRead (Cache.audio_path chart backbeat_cache).Value
+                            )
+
+                        response.EnsureSuccessStatusCode() |> ignore
+
+                        Logging.Info(
+                            sprintf
+                                "Uploaded %s (%.1fMB)"
+                                audio_hash
+                                (float response.Response.ContentLength / 1000000.0)
+                        )
+
+                        add_existing_file audio_hash
+                }
+                |> Async.AwaitTask
+
+                task {
+                    let bg_hash = "assets/" + chart_info.BackgroundFile
+
+                    let exists = existing_files.ContainsKey bg_hash
+
+                    if not exists then
+                        let! response =
+                            client.UploadAsync(
+                                "c44023fe407a500583900717",
+                                bg_hash,
+                                File.OpenRead (Cache.background_path chart backbeat_cache).Value
+                            )
+
+                        response.EnsureSuccessStatusCode() |> ignore
+
+                        Logging.Info(
+                            sprintf "Uploaded %s (%.1fMB)" bg_hash (float response.Response.ContentLength / 1000000.0)
+                        )
+
+                        add_existing_file bg_hash
+                }
+                |> Async.AwaitTask
+            ]
+        |> Async.Ignore
 
     let private mass_upload_service =
         let sub_services =
-            Array.init 24
+            Array.init
+                24
                 (fun i ->
                     { new Async.Service<(Interlude.Chart * Chart), unit>() with
-                        override this.Handle((chart, chart_info)) = 
+                        override this.Handle((chart, chart_info)) =
                             async {
                                 do! upload_chart_internal chart chart_info
-                                if i = 0 && this.Status = Async.ServiceStatus.Working then Logging.Debug(sprintf "Worker #%i completed its last upload" i)
+
+                                if i = 0 && this.Status = Async.ServiceStatus.Working then
+                                    Logging.Debug(sprintf "Worker #%i completed its last upload" i)
                             }
                     }
                 )
+
         let mutable i = 0
+
         { new Async.Service<(Interlude.Chart * Chart), unit>() with
             override this.Handle(r) =
                 async {
@@ -110,4 +153,5 @@ module Upload =
                 }
         }
 
-    let upload_chart (chart: Interlude.Chart) (chart_info: Chart) = mass_upload_service.Request((chart, chart_info), ignore)
+    let upload_chart (chart: Interlude.Chart) (chart_info: Chart) =
+        mass_upload_service.Request((chart, chart_info), ignore)
