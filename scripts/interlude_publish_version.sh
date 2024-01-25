2f0ec69f7cf82ee0918cea0b26c5d117305d1761
percyqaz <21290233+percyqaz@users.noreply.github.com>
2024-01-25 02:07:22 +0000

    ⚡️ Fix release script

--- scripts/interlude_publish_version.sh
+++ scripts/interlude_publish_version.sh
@@ -1,6 +1,10 @@
-cd ../interlude/tools && dotnet build --configuration Debug && cd bin/Debug/net8.0 && clear && dotnet run --project ../../.. -- publish
-cd ../../scripts
-./generate_site.sh
-git add site
+cd ../interlude/tools
+dotnet build --configuration Debug -v q
+cd bin/Debug/net8.0
+clear
+dotnet run --project ../../.. -- publish
+cd ../../../../../scripts
+./site_generator.sh
+git add ../site
 git commit -m "📘 Update site changelog & wiki"
 git push
