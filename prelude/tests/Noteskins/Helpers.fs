61f058446845a713ed34c0bf2c6f8526008b2757
percyqaz <21290233+percyqaz@users.noreply.github.com>
2024-03-03 18:30:15 +0000

    ⚡️ Test rig for noteskin validation

---
+++ prelude/tests/Noteskins/Helpers.fs
@@ -0,0 +1,41 @@
+﻿namespace Prelude.Tests.Noteskins
+
+open NUnit.Framework
+open System
+open System.IO
+open System.IO.Compression
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+open Percyqaz.Common
+open Percyqaz.Data
+open Prelude.Common
+open Prelude.Data.Content
+
+module Helpers =
+    
+    type InMemoryNoteskinBuilder(config: NoteskinConfig) =
+        let memory_stream = new MemoryStream()
+        let zip_archive = new ZipArchive(memory_stream, ZipArchiveMode.Create, true)
+
+        do
+            let config_file = zip_archive.CreateEntry("noteskin.json")
+            use stream = config_file.Open()
+            JSON.ToStream stream config
+
+        member this.AddJsonFile(path: string, data: 'T) : InMemoryNoteskinBuilder =
+            let entry = zip_archive.CreateEntry(path)
+            use stream = entry.Open()
+            JSON.ToStream stream data
+            this
+        
+        member this.AddImageFile(path: string, image: Bitmap) : InMemoryNoteskinBuilder =
+            let entry = zip_archive.CreateEntry(path)
+            use stream = entry.Open()
+            image.SaveAsPng stream
+            this
+
+        member this.Build() : Noteskin =
+            zip_archive.Dispose()
+            memory_stream.Position <- 0
+
+            Noteskin.FromZipStream(memory_stream)
---
+++ prelude/tests/Noteskins/Validation.fs
@@ -0,0 +1,186 @@
+﻿namespace Prelude.Tests.Noteskins
+
+open NUnit.Framework
+open Prelude.Common
+open Prelude.Data.Content
+open Helpers
+
+module Validation =
+
+    let ONEPIXELIMAGE = new Bitmap(1, 1)
+
+    [<Test>]
+    let MissingTextures () =
+        let noteskin = 
+            InMemoryNoteskinBuilder(NoteskinConfig.Default)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .Build()
+
+        let validation_results =
+            noteskin.Validate()
+            |> Array.ofSeq
+
+        printfn "%A" validation_results
+
+        Assert.AreEqual(2, validation_results.Length)
+
+        match Array.tryFind (function ValidationError { Element = "note" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected an error message for missing 'note' texture")
+        | _ -> ()
+
+        match Array.tryFind (function ValidationError { Element = "receptor" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected an error message for missing 'receptor' texture")
+        | _ -> ()
+
+    [<Test>]
+    let WithRequiredTextures () =
+        let noteskin = 
+            InMemoryNoteskinBuilder(NoteskinConfig.Default)
+                .AddImageFile("note.png", ONEPIXELIMAGE)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddImageFile("receptor.png", ONEPIXELIMAGE)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .Build()
+    
+        let validation_results = 
+            noteskin.Validate()
+            |> Array.ofSeq
+
+        printfn "%A" validation_results
+    
+        Assert.AreEqual(0, validation_results.Length)
+
+    [<Test>]
+    let WithExtraTextures () =
+        let noteskin = 
+            InMemoryNoteskinBuilder({ NoteskinConfig.Default with EnableColumnLight = false })
+                .AddImageFile("note.png", ONEPIXELIMAGE)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddImageFile("receptor.png", ONEPIXELIMAGE)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .AddImageFile("noteexplosion.png", ONEPIXELIMAGE)
+                .Build()
+        
+        let validation_results = 
+            noteskin.Validate()
+            |> Array.ofSeq
+    
+        printfn "%A" validation_results
+        
+        Assert.AreEqual(2, validation_results.Length)
+
+        match Array.tryFind (function ValidationWarning { Element = "noteexplosion" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected a warning message for unnecessary 'noteexplosion' texture")
+        | _ -> ()
+
+        match Array.tryFind (function ValidationWarning { Element = "receptorlighting" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected a warning message for unnecessary 'receptorlighting' texture")
+        | _ -> ()
+    
+    [<Test>]
+    let WithWrongNoteTextureModeLoose () =
+        let noteskin = 
+            InMemoryNoteskinBuilder(NoteskinConfig.Default)
+                .AddJsonFile("note.json", { Columns = 1; Rows = 1; Mode = Loose })
+                .AddImageFile("note.png", ONEPIXELIMAGE)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddImageFile("receptor.png", ONEPIXELIMAGE)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .Build()
+        
+        let validation_results = 
+            noteskin.Validate()
+            |> Array.ofSeq
+    
+        printfn "%A" validation_results
+        
+        Assert.AreEqual(1, validation_results.Length)
+
+        match Array.tryFind (function ValidationError { Element = "note" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected an error message for wrong mode on 'note' texture")
+        | _ -> ()
+        
+    [<Test>]
+    let WithWrongNoteTextureModeGrid () =
+        let noteskin = 
+            InMemoryNoteskinBuilder(NoteskinConfig.Default)
+                .AddImageFile("note-0-0.png", ONEPIXELIMAGE)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddImageFile("receptor.png", ONEPIXELIMAGE)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .Build()
+            
+        let validation_results = 
+            noteskin.Validate()
+            |> Array.ofSeq
+        
+        printfn "%A" validation_results
+            
+        Assert.AreEqual(1, validation_results.Length)
+
+        match Array.tryFind (function ValidationError { Element = "note" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected an error message for wrong mode on 'note' texture")
+        | _ -> ()
+
+    [<Test>]
+    let WithTooManyRows () =
+        use receptor_grid_bmp = new Bitmap(16, 3)
+        let noteskin = 
+            InMemoryNoteskinBuilder(NoteskinConfig.Default)
+                .AddImageFile("note.png", ONEPIXELIMAGE)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddJsonFile("receptor.json", { Columns = 16; Rows = 3; Mode = Grid })
+                .AddImageFile("receptor.png", receptor_grid_bmp)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .Build()
+        
+        let validation_results = 
+            noteskin.Validate()
+            |> Array.ofSeq
+    
+        printfn "%A" validation_results
+        
+        Assert.AreEqual(1, validation_results.Length)
+
+        match Array.tryFind (function ValidationError { Element = "receptor" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected an error message for row count on 'receptor' texture")
+        | _ -> ()
+
+    [<Test>]
+    let WithWrongGridImageDimensions () =
+        use receptor_grid_bmp = new Bitmap(16, 24)
+        let noteskin = 
+            InMemoryNoteskinBuilder({ NoteskinConfig.Default with ReceptorStyle = ReceptorStyle.Rotate })
+                .AddImageFile("note.png", ONEPIXELIMAGE)
+                .AddImageFile("holdbody.png", ONEPIXELIMAGE)
+                .AddImageFile("holdhead.png", ONEPIXELIMAGE)
+                .AddImageFile("holdtail.png", ONEPIXELIMAGE)
+                .AddJsonFile("receptor.json", { Columns = 16; Rows = 2; Mode = Grid })
+                .AddImageFile("receptor.png", receptor_grid_bmp)
+                .AddImageFile("receptorlighting.png", ONEPIXELIMAGE)
+                .Build()
+        
+        let validation_results = 
+            noteskin.Validate()
+            |> Array.ofSeq
+    
+        printfn "%A" validation_results
+        
+        Assert.AreEqual(1, validation_results.Length)
+
+        match Array.tryFind (function ValidationError { Element = "receptor" } -> true | _ -> false) validation_results with
+        | None -> Assert.Fail("Expected an error message for image size on 'receptor' texture")
+        | _ -> ()
