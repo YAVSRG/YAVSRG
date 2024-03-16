dcac08a6d6040cef01f2da66bc8afcca990fd811
percyqaz <21290233+percyqaz@users.noreply.github.com>
2024-03-16 22:45:08 +0000

    🌸 Truncate minor details from pattern analysis

--- interlude/src/Features/LevelSelect/Info/Details.fs
+++ interlude/src/Features/LevelSelect/Info/Details.fs
@@ -136,4 +136,4 @@ type Patterns(display: Setting<Display>) =
 
     member this.OnChartUpdated(info: Chart.LoadedChartInfo) =
         patterns <- info.Patterns.Patterns |> List.truncate 6
-        category <- info.Patterns.Category
+        category <- { info.Patterns.Category with MinorFeatures = info.Patterns.Category.MinorFeatures |> List.truncate 3 }
