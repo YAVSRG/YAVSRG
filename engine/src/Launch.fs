332649078176102ce51b52fbfe7b8da1f74da999
Percy Qaz <percyqaz@yahoo.co.uk>
2024-03-27 22:09:33 +0000

    👽️ Add error message mentioning vc++ redist

--- engine/src/Launch.fs
+++ engine/src/Launch.fs
@@ -33,7 +33,13 @@ module Launch =
 
                 Some w
             with err ->
-                Logging.Critical(name + " failed to launch", err)
+                match err with
+                | :? DllNotFoundException ->
+                    Logging.Critical(
+                        "\n====\nEngine failed to load a DLL! Please check that:\n - You didn't delete one of the DLLs that came with the exe\n - You have VC++ redist installed - https://aka.ms/vs/16/release/vc_redist.x64.exe\nIf the program still won't start, please contact Percyqaz!\n====",
+                        err
+                    )
+                | _ -> Logging.Critical(name + " failed to launch", err)
                 Console.ReadLine() |> ignore
                 None
 
