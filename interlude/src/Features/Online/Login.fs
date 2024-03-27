dd445edd6f5b9f537cbb3a4bd2ac35ad187abdd9
Percy Qaz <percyqaz@yahoo.co.uk>
2024-03-27 23:06:17 +0000

    🌸 Fix layout of signup page

--- interlude/src/Features/Online/Login.fs
+++ interlude/src/Features/Online/Login.fs
@@ -28,7 +28,7 @@ type RegisterPage(discord_tag) as this =
             .Body("Choose your username wisely! This is what gets displayed on your profile.")
             .Body("Usernames must be 3-20 characters long, and can have at most 2 special characters")
             .Body(
-                "By completing your registration you confirm that:\n You have read, understood and agree to abide by Interlude's Terms of Service and Privacy Policy\n You are 18 years old or older"
+                "By completing your registration you confirm that:\n - You have read, understood and agree to abide by Interlude's Terms of Service and Privacy Policy\n - You are 18 years old or older"
             )
 
     do
@@ -37,17 +37,17 @@ type RegisterPage(discord_tag) as this =
             |+ PageTextEntry("register.username", username).Pos(2)
             |+ Text(
                 "Creating an account linked to " + discord_tag,
-                Position = pretty_pos(0, 2, PageWidth.Full),
+                Position = pretty_pos(0, 2, PageWidth.Full).Margin(0.0f, 10.0f),
                 Align = Alignment.LEFT
             )
-            |+ Callout.frame info (fun (w, h) -> pretty_pos(3, 5, PageWidth.Custom w))
+            |+ Callout.frame info (fun (w, h) -> pretty_pos(4, 9, PageWidth.Custom w).Translate(0.0f, 25.0f))
             |+ PageButton("register.terms_of_service", (fun () -> open_url ("https://yavsrg.net/terms_of_service")))
-                .Pos(9)
+                .Pos(14)
             |+ PageButton("register.privacy_policy", (fun () -> open_url ("https://yavsrg.net/privacy_policy")))
-                .Pos(11)
+                .Pos(16)
             |+ PageSetting("register.confirm_terms_of_service", Selector<_>.FromBool agree_tos)
-                .Pos(13)
-            |+ submit_button.Pos(16)
+                .Pos(18)
+            |+ submit_button.Pos(21)
         )
 
     override this.Title = %"register.name"
@@ -90,7 +90,7 @@ type LoginPage() as this =
             |+ PageButton("login.register_with_discord", register).Pos(3)
             |+ Conditional(
                 (fun () -> waiting_for_browser),
-                Callout.frame info (fun (w, h) -> Position.Row(400.0f, h).Margin(100.0f, 0.0f))
+                Callout.frame info (fun (w, h) -> Position.Row(400.0f, h))
             )
         )
 
