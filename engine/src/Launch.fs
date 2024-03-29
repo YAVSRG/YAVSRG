7c9586ef5e3e795bcfda793ffd44e5fcfe692077
Percy Qaz <percyqaz@yahoo.co.uk>
2024-03-29 16:32:40 +0000

    📘 Update VCredist link for users missing it

--- engine/src/Launch.fs
+++ engine/src/Launch.fs
@@ -36,7 +36,7 @@ module Launch =
                 match err with
                 | :? DllNotFoundException ->
                     Logging.Critical(
-                        "\n====\nEngine failed to load a DLL! Please check that:\n - You didn't delete one of the DLLs that came with the exe\n - You have VC++ redist installed - https://aka.ms/vs/16/release/vc_redist.x64.exe\nIf the program still won't start, please contact Percyqaz!\n====",
+                        "\n====\nEngine failed to load a DLL! Please check that:\n - You didn't delete one of the DLLs that came with the exe\n - You have VC++ redist installed - https://aka.ms/vs/17/release/vc_redist.x64.exe\nIf the program still won't start, please contact Percyqaz!\n====",
                         err
                     )
                 | _ -> Logging.Critical(name + " failed to launch", err)
