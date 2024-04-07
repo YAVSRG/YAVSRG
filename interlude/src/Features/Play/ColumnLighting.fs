5389ac1eb56c86dc6b2cc3857a9cd9c4d14becdb
Percy Qaz <percyqaz@yahoo.co.uk>
2024-04-07 11:21:09 +0100

    🧹 Split some playfield elements into separate files

---
+++ interlude/src/Features/Play/ColumnLighting.fs
@@ -0,0 +1,86 @@
+﻿namespace Interlude.Features.Play
+
+open Percyqaz.Flux.UI
+open Percyqaz.Flux.Graphics
+open Prelude
+open Prelude.Content.Noteskins
+open Interlude.Options
+open Interlude.Content
+open Interlude.Features.Play
+
+type ColumnLighting(keys, ns: NoteskinConfig, state) as this =
+    inherit StaticWidget(NodeType.None)
+    let timers = Array.init keys (fun _ -> Animation.Delay ns.ColumnLightDuration)
+    let sprite = Content.Texture "receptorlighting"
+
+    let column_spacing = ns.KeymodeColumnSpacing keys
+
+    let column_positions =
+        let mutable x = 0.0f
+
+        Array.init
+            keys
+            (fun i ->
+                let v = x
+
+                if i + 1 < keys then
+                    x <- x + ns.ColumnWidth + column_spacing.[i]
+
+                v
+            )
+
+    do
+        let hitpos = float32 options.HitPosition.Value
+
+        this.Position <-
+            { Position.Default with
+                Top = 0.0f %+ hitpos
+                Bottom = 1.0f %- hitpos
+            }
+
+    override this.Update(elapsed_ms, moved) =
+        base.Update(elapsed_ms, moved)
+        timers |> Array.iter (fun s -> s.Update elapsed_ms)
+
+        Array.iteri
+            (fun k (s: Animation.Delay) ->
+                if state.Scoring.KeyState |> Bitmask.has_key k then
+                    s.Reset()
+            )
+            timers
+
+    override this.Draw() =
+
+        let draw_column k (s: Animation.Delay) =
+            if not s.Complete then
+                let percent_remaining =
+                    1.0f - float32 (s.Elapsed / s.Interval) |> min 1.0f |> max 0.0f
+
+                let a = 255.0f * percent_remaining |> int
+
+                Draw.quad
+                    (let x = ns.ColumnWidth * 0.5f + column_positions.[k]
+
+                     if options.Upscroll.Value then
+                         Sprite.aligned_box_x
+                             (this.Bounds.Left + x,
+                              this.Bounds.Top,
+                              0.5f,
+                              1.0f,
+                              ns.ColumnWidth * percent_remaining,
+                              -1.0f / percent_remaining)
+                             sprite
+                     else
+                         Sprite.aligned_box_x
+                             (this.Bounds.Left + x,
+                              this.Bounds.Bottom,
+                              0.5f,
+                              1.0f,
+                              ns.ColumnWidth * percent_remaining,
+                              1.0f / percent_remaining)
+                             sprite)
+                        .AsQuad
+                    (Quad.color (Color.FromArgb(a, Color.White)))
+                    (Sprite.pick_texture (0, k) sprite)
+
+        Array.iteri draw_column timers
---
+++ interlude/src/Features/Play/LaneCover.fs
@@ -0,0 +1,48 @@
+﻿namespace Interlude.Features.Play
+
+open Percyqaz.Flux.UI
+open Percyqaz.Flux.Graphics
+open Prelude
+open Interlude.Options
+
+type LaneCover() =
+    inherit StaticWidget(NodeType.None)
+
+    override this.Draw() =
+
+        if options.LaneCover.Enabled.Value then
+
+            let bounds = this.Bounds.Expand(0.0f, 2.0f)
+            let fade_length = options.LaneCover.FadeLength.Value
+
+            let upper (amount: float32) =
+                Draw.rect (bounds.SliceTop(amount - fade_length)) options.LaneCover.Color.Value
+
+                Draw.untextured_quad
+                    (bounds.SliceTop(amount).SliceBottom(fade_length).AsQuad)
+                    struct (options.LaneCover.Color.Value,
+                            options.LaneCover.Color.Value,
+                            Color.FromArgb(0, options.LaneCover.Color.Value),
+                            Color.FromArgb(0, options.LaneCover.Color.Value))
+
+            let lower (amount: float32) =
+                Draw.rect (bounds.SliceBottom(amount - fade_length)) options.LaneCover.Color.Value
+
+                Draw.untextured_quad
+                    (bounds.SliceBottom(amount).SliceTop(fade_length).AsQuad)
+                    struct (Color.FromArgb(0, options.LaneCover.Color.Value),
+                            Color.FromArgb(0, options.LaneCover.Color.Value),
+                            options.LaneCover.Color.Value,
+                            options.LaneCover.Color.Value)
+
+            let height = bounds.Height
+
+            let sudden = options.LaneCover.Sudden.Value * height
+            let hidden = options.LaneCover.Hidden.Value * height
+
+            if options.Upscroll.Value then
+                upper hidden
+                lower sudden
+            else
+                lower hidden
+                upper sudden
