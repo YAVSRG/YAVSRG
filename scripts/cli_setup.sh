ab4743dbb61f79a13140a1b9165f3dfa78072c7e
Percy Qaz <percyqaz@yahoo.co.uk>
2024-04-26 14:57:15 +0100

    ⚡️ Better-than-nothing script for linux and macos players

--- scripts/cli_setup.sh
+++
@@ -1,3 +0,0 @@
-cd ../interlude/tools
-dotnet pack
-dotnet tool install -g --add-source ./nupkg Interlude.Tools
---
+++ scripts/linux_macos_players.sh
@@ -0,0 +1,8 @@
+﻿git checkout main
+git pull
+git fetch --tags
+git checkout $(git describe --tags "$(git rev-list --tags --max-count=1)")
+chmod +x repo_setup.sh
+./repo_setup.sh
+chmod +x interlude_run.sh
+./interlude_run.sh
