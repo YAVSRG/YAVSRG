ee692edc7ceffa46fa59b3e0b972982ab23f88e2
Percy Qaz <percyqaz@yahoo.co.uk>
2024-05-25 00:14:37 +0100

    🌸 Improve look of game mounting cards

--- interlude/src/Features/Import/Mounts/Mounts.fs
+++ interlude/src/Features/Import/Mounts/Mounts.fs
@@ -66,9 +66,9 @@ module Mounts =
 
     let tab =
         NavigationContainer.Column<Widget>()
-        |+ MountControl(MountedGameType.Osu, options.OsuMount, Position = Position.Row(100.0f, 150.0f))
-        |+ MountControl(MountedGameType.Quaver, options.QuaverMount, Position = Position.Row(270.0f, 150.0f))
-        |+ MountControl(MountedGameType.Stepmania, options.StepmaniaMount, Position = Position.Row(440.0f, 150.0f))
-        |+ MountControl(MountedGameType.Etterna, options.EtternaMount, Position = Position.Row(610.0f, 150.0f))
+        |+ MountControl(MountedGameType.Osu, options.OsuMount, Position = Position.Row(100.0f, 150.0f).Margin(200.0f, 0.0f))
+        |+ MountControl(MountedGameType.Quaver, options.QuaverMount, Position = Position.Row(270.0f, 150.0f).Margin(200.0f, 0.0f))
+        |+ MountControl(MountedGameType.Stepmania, options.StepmaniaMount, Position = Position.Row(440.0f, 150.0f).Margin(200.0f, 0.0f))
+        |+ MountControl(MountedGameType.Etterna, options.EtternaMount, Position = Position.Row(610.0f, 150.0f).Margin(200.0f, 0.0f))
         |+ Text(%"imports.mount", Align = Alignment.CENTER, Position = Position.Row(0.0f, 80.0f))
         |+ Text(%"imports.drag_and_drop_hint", Align = Alignment.CENTER, Position = Position.SliceBottom(80.0f).Translate(0.0f, -10.0f))
