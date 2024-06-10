e04aeae78ce387e49adaaf12d2130f786289bfd0
Percy Qaz <percyqaz@yahoo.co.uk>
2024-06-10 13:04:16 +0100

    💚 Remove stock rulesets from rulesets list

--- tools/Content/Rulesets.fs
+++ tools/Content/Rulesets.fs
@@ -14,11 +14,6 @@ module Rulesets =
 
     let DEFAULT_RULESETS =
         List.map
-            (fun od -> (sprintf "osu-od-%.0f" od, ``osu!``.create od ``osu!``.NoMod ))
-            [ 0.0f; 1.0f; 2.0f; 3.0f; 4.0f; 5.0f; 6.0f; 7.0f; 8.0f; 9.0f; 10.0f ]
-        @ List.map (fun j -> (sprintf "sc-j%i" j, SC.create j)) [ 2; 3; 4; 5; 6; 7; 8; 9 ]
-        @ List.map (fun j -> (sprintf "wife-j%i" j, Wife3.create j)) [ 2; 3; 4; 5; 6; 7; 8; 9 ]
-        @ List.map
             (fun (d: Ex_Score.Type) ->
                 (sprintf "xs-%s" (d.Name.ToLower()), Ex_Score.create d)
             )
