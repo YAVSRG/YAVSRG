b01a1085cb4d89fc95779930a902b54923063555
Percy Qaz <percyqaz@yahoo.co.uk>
2024-08-06 07:56:04 +0100

    🌸 Rearrange chart info

--- interlude/src/Features/LevelSelect/Info/Components.fs
+++ interlude/src/Features/LevelSelect/Info/Components.fs
@@ -126,18 +126,18 @@ type BottomInfo() =
         base.Init parent
 
     override this.Draw() =
+        
+        let play_info = this.Bounds.SliceT(40.0f).TranslateY(90.0f).ShrinkX(15.0f)
+        Text.fill_b (Style.font, Mods.format (SelectedChart.rate.Value, SelectedChart.selected_mods.Value, SelectedChart.autoplay), play_info, Colors.text, Alignment.LEFT)
+        Text.fill_b (Style.font, "Last played 3d ago", play_info, Colors.text, Alignment.RIGHT)
 
-        let three_icon_infos = this.Bounds.SliceT(70.0f).TranslateY(90.0f).ShrinkX(15.0f)
+        let chart_info = this.Bounds.SliceT(30.0f).TranslateY(130.0f).ShrinkX(15.0f)
+        Text.fill_b (Style.font, (match SelectedChart.CACHE_DATA with Some cc -> cc.DifficultyName | None -> ""), chart_info.SlicePercentL 0.5f, Colors.text_subheading, Alignment.LEFT)
+        Text.fill_b (Style.font, notecounts, chart_info, Colors.text_subheading, Alignment.RIGHT)
+
+        let three_icon_infos = this.Bounds.SliceT(70.0f).TranslateY(155.0f).ShrinkX(15.0f)
         Text.fill_b (Style.font, sprintf "%s %.2f" Icons.STAR rating, three_icon_infos, (Colors.white, DifficultyRating.physical_color rating), Alignment.LEFT)
         Text.fill_b (Style.font, SelectedChart.FMT_BPM, three_icon_infos, Colors.text, Alignment.CENTER)
         Text.fill_b (Style.font, SelectedChart.FMT_DURATION, three_icon_infos, Colors.text, Alignment.RIGHT)
-
-        let chart_info = this.Bounds.SliceT(30.0f).TranslateY(160.0f).ShrinkX(15.0f)
-        Text.fill_b (Style.font, (match SelectedChart.CACHE_DATA with Some cc -> cc.DifficultyName | None -> ""), chart_info.SlicePercentL 0.5f, Colors.text_subheading, Alignment.LEFT)
-        Text.fill_b (Style.font, notecounts, chart_info, Colors.text_subheading, Alignment.RIGHT)
-        
-        let play_info = this.Bounds.SliceT(40.0f).TranslateY(185.0f).ShrinkX(15.0f)
-        Text.fill_b (Style.font, Mods.format (SelectedChart.rate.Value, SelectedChart.selected_mods.Value, SelectedChart.autoplay), play_info, Colors.text, Alignment.LEFT)
-        Text.fill_b (Style.font, "Last played 3d ago", play_info, Colors.text, Alignment.RIGHT)
 
         base.Draw()
