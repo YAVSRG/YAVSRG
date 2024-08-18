8ca1152740cd3618e384c8f630a389c3b98ce11b
Percy Qaz <percyqaz@yahoo.co.uk>
2024-08-19 00:20:33 +0100

    🌸 Show more of HUD when watching autoplay

--- interlude/src/Features/Play/Replay/ReplayScreen.fs
+++ interlude/src/Features/Play/Replay/ReplayScreen.fs
@@ -64,9 +64,9 @@ module ReplayScreen =
                     if hud_config.JudgementCounterEnabled then add_widget hud_config.JudgementCounterPosition JudgementCounter
                     if hud_config.JudgementMeterEnabled then add_widget hud_config.JudgementMeterPosition JudgementMeter
                     if hud_config.EarlyLateMeterEnabled then add_widget hud_config.EarlyLateMeterPosition EarlyLateMeter
-                    if hud_config.RateModMeterEnabled then add_widget hud_config.RateModMeterPosition RateModMeter
-                    if hud_config.BPMMeterEnabled then add_widget hud_config.BPMMeterPosition BPMMeter
-                    if hud_config.InputMeterEnabled then add_widget hud_config.InputMeterPosition InputMeter
+                if hud_config.RateModMeterEnabled then add_widget hud_config.RateModMeterPosition RateModMeter
+                if hud_config.BPMMeterEnabled then add_widget hud_config.BPMMeterPosition BPMMeter
+                if hud_config.InputMeterEnabled then add_widget hud_config.InputMeterPosition InputMeter
                 if hud_config.CustomImageEnabled then add_widget hud_config.CustomImagePosition CustomImage
 
                 this
