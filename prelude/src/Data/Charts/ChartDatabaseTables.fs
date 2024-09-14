50fceac9ef65919df95b21ae847985c2d7d1edcf
Percy Qaz <percyqaz@yahoo.co.uk>
2024-09-14 18:38:53 +0100

    🧱 Adjustments to chart database for future proofing

--- prelude/src/Data/Charts/ChartDatabaseTables.fs
+++ prelude/src/Data/Charts/ChartDatabaseTables.fs
@@ -40,7 +40,7 @@ type DbChart =
         Audio: DbAssetPath
         PreviewTime: Time
 
-        Folder: string
+        Folders: string list
         Origin: DbChartOrigin
 
         Keys: int
@@ -48,6 +48,7 @@ type DbChart =
         BPM: int
         DateAdded: int64
         Rating: float32
+        Patterns: PatternSummary.Info
     }
     static member FromChart (file_timestamp: int64) (chart: Chart) =
         let source_folder_path = Path.GetDirectoryName(chart.LoadedFromPath)
@@ -79,7 +80,7 @@ type DbChart =
                 | MediaPath.Missing -> DbAssetPath.Missing
             PreviewTime = chart.Header.PreviewTime
 
-            Folder = source_folder_name
+            Folders = [ source_folder_name ]
             Origin = 
                 match chart.Header.ChartSource with
                 | Origin.Osu (set, map) -> DbChartOrigin.Osu(set, map)
@@ -98,9 +99,13 @@ type DbChart =
                 else 0
             DateAdded = file_timestamp
             Rating = (DifficultyRating.calculate 1.0f chart.Notes).Physical |> float32
+            Patterns = (PatternSummary.generate_pattern_data_uncached 1.0f chart)
         }
 
 module DbCharts =
+
+    // Increment this to recalculate pattern & rating data
+    let internal CALC_VERSION = 0uy
 
     let internal CREATE_TABLE: NonQuery<unit> =
         { NonQuery.without_parameters () with
@@ -120,13 +125,15 @@ module DbCharts =
                 Background TEXT NOT NULL,
                 Audio TEXT NOT NULL,
                 PreviewTime REAL NOT NULL,
-                Folder TEXT NOT NULL,
+                Folders TEXT NOT NULL,
                 Origin TEXT NOT NULL,
                 Keys INTEGER NOT NULL,
                 Length REAL NOT NULL,
                 BPM INTEGER NOT NULL,
                 DateAdded INTEGER NOT NULL,
+                CalcVersion INTEGER NOT NULL,
                 Rating REAL NOT NULL,
+                Patterns TEXT NOT NULL,
                 Chart BLOB NOT NULL
             );
             """
@@ -136,8 +143,37 @@ module DbCharts =
         {
             SQL =
                 """
-                INSERT OR REPLACE INTO charts (Id, Title, TitleNative, Artist, ArtistNative, DifficultyName, Subtitle, Source, Creator, Tags, Background, Audio, PreviewTime, Folder, Origin, Keys, Length, BPM, DateAdded, Rating, Chart)
-                VALUES (@Id, @Title, @TitleNative, @Artist, @ArtistNative, @DifficultyName, @Subtitle, @Source, @Creator, @Tags, @Background, @Audio, @PreviewTime, @Folder, @Origin, @Keys, @Length, @BPM, @DateAdded, @Rating, @Chart);
+                INSERT INTO charts (
+                    Id,
+                    Title, TitleNative, Artist, ArtistNative,
+                    DifficultyName, Subtitle, Source, Creator, Tags,
+                    Background, Audio, PreviewTime,
+                    Folders, Origin,
+                    Keys, Length, BPM,
+                    CalcVersion, DateAdded, Rating, Patterns,
+                    Chart
+                )
+                VALUES (
+                    @Id,
+                    @Title, @TitleNative, @Artist, @ArtistNative,
+                    @DifficultyName, @Subtitle, @Source, @Creator, @Tags,
+                    json(@Background), json(@Audio), @PreviewTime,
+                    @Folders, json(@Origin),
+                    @Keys, @Length, @BPM,
+                    @CalcVersion, @DateAdded, @Rating, json(@Patterns),
+                    @Chart)
+                ON CONFLICT DO UPDATE SET
+                    DateAdded = excluded.DateAdded,
+                    Folders = (
+                        SELECT json_group_array(value) FROM (
+                            SELECT json_each.value
+                            FROM charts, json_each(charts.Folders)
+                            WHERE charts.Id = excluded.Id
+                            UNION
+                            SELECT json_each.value
+                            FROM json_each(excluded.Folders)
+                        ) GROUP BY ''
+                    );
             """
             Parameters =
                 [
@@ -154,13 +190,15 @@ module DbCharts =
                     "@Background", SqliteType.Text, -1
                     "@Audio", SqliteType.Text, -1
                     "@PreviewTime", SqliteType.Real, 4
-                    "@Folder", SqliteType.Text, -1
+                    "@Folders", SqliteType.Text, -1
                     "@Origin", SqliteType.Text, -1
                     "@Keys", SqliteType.Integer, 1
                     "@Length", SqliteType.Real, 4
                     "@BPM", SqliteType.Integer, 4
                     "@DateAdded", SqliteType.Integer, 8
+                    "@CalcVersion", SqliteType.Integer, 1
                     "@Rating", SqliteType.Real, 8
+                    "@Patterns", SqliteType.Text, -1
                     "@Chart", SqliteType.Blob, -1
                 ]
             FillParameters =
@@ -178,16 +216,18 @@ module DbCharts =
                     p.Json JSON db_chart.Background
                     p.Json JSON db_chart.Audio
                     p.Float32 (float32 db_chart.PreviewTime)
-                    p.String db_chart.Folder
+                    p.Json JSON db_chart.Folders
                     p.Json JSON db_chart.Origin
                     p.Byte (byte db_chart.Keys)
                     p.Float32 (float32 db_chart.Length)
                     p.Int32 db_chart.BPM
+                    p.Byte CALC_VERSION
                     p.Int64 db_chart.DateAdded
                     p.Float32 db_chart.Rating
+                    p.Json JSON db_chart.Patterns
                     p.Blob (
-                        use ms = new System.IO.MemoryStream()
-                        use bw = new System.IO.BinaryWriter(ms)
+                        use ms = new MemoryStream()
+                        use bw = new BinaryWriter(ms)
                         Chart.write_headless chart_data bw
                         ms.ToArray()
                    )
