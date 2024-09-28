f9527dbb58770a3a2e657b4b69b86b1ff59ddc72
Percy Qaz <percyqaz@yahoo.co.uk>
2024-09-28 23:16:54 +0100

    ⚡️ Ruleset validation + start of test rig

--- prelude/src/Gameplay/RulesetsRewrite/Rulesets.fs
+++ prelude/src/Gameplay/RulesetsRewrite/Rulesets.fs
@@ -36,6 +36,12 @@ type Grade =
 type LampRequirement =
     | JudgementAtMost of judgement_id: int * count: int
     | ComboBreaksAtMost of count: int
+    member this.IsStricterThan(other: LampRequirement) =
+        match this, other with
+        | JudgementAtMost _, ComboBreaksAtMost _ -> true
+        | ComboBreaksAtMost _, JudgementAtMost _ -> false
+        | ComboBreaksAtMost n, ComboBreaksAtMost m -> n < m
+        | JudgementAtMost (j, n), JudgementAtMost (j2, m) -> j < j2 || (j = j2 && n < m)
 
 [<Json.AutoCodec>]
 type Lamp =
@@ -230,5 +236,90 @@ module RulesetV2 =
         let hash_string = ms.ToArray() |> h.ComputeHash |> BitConverter.ToString
         first_character + hash_string.Replace("-", "").Substring(0, 8)
 
-    let check (ruleset: RulesetV2) : Result<unit, string> =
-        Error "Not yet implemented"
+    let check (ruleset: RulesetV2) : Result<RulesetV2, string> =
+        let inline valid (x: GameplayTime) = x |> float32 |> Single.IsFinite
+        let invalid = valid >> not
+        let inline negative (x: GameplayTime) = invalid x || x < 0.0f<ms / rate>
+        let inline positive (x: GameplayTime) = invalid x || x > 0.0f<ms / rate>
+
+        try
+            if ruleset.Judgements.Length = 0 then failwith "Must have at least one judgement"
+
+            let mutable w_min = 0.0f<ms / rate>
+            let mutable w_max = 0.0f<ms / rate>
+            for j in ruleset.Judgements do
+                match j.TimingWindows with
+                | Some (early, late) ->
+                    if invalid(early) then failwithf "Invalid floating point early window for '%s'" j.Name
+                    if early > w_min then failwithf "Early window %.3fms for '%s' must be %.3fms or earlier" early j.Name w_min
+                    w_min <- early
+                    if invalid(late) then failwithf "Invalid floating point late window for '%s'" j.Name
+                    if late < w_max then failwithf "Late window %.3fms for '%s' must be %.3fms or late" late j.Name w_max
+                    w_max <- late
+                | None -> ()
+
+            let mutable g_acc = -infinity
+            for g in ruleset.Grades do
+                if g.Accuracy <= g_acc then failwithf "Grade boundary %.6f for '%s' must be at least %.6f" g.Accuracy g.Name g_acc
+                g_acc <- g.Accuracy
+
+            let mutable l_req = LampRequirement.ComboBreaksAtMost Int32.MaxValue
+            for l in ruleset.Lamps do
+                if not (l.Requirement.IsStricterThan l_req) then
+                    failwithf "Lamp requirement %A for '%s' must be stricter than %A" l.Requirement l.Name l_req
+                l_req <- l.Requirement
+
+            match ruleset.HitMechanics with
+            | HitMechanics.OsuMania -> ()
+            | HitMechanics.Interlude cbrush_threshold ->
+                if cbrush_threshold < 0.0f<ms / rate> then failwith "Interlude `cbrush_threshold` must be non-negative"
+            | HitMechanics.Etterna -> ()
+
+            match ruleset.HoldMechanics with
+            | HoldMechanics.CombineHeadAndTail rule ->
+                match rule with
+                | HeadTailCombineRule.OsuMania windows ->
+                    
+                    if negative(windows.Window320) then failwith "osu!mania ln Window320 must be non-negative"
+                    if negative(windows.Window300) then failwith "osu!mania ln Window300 must be non-negative"
+                    if negative(windows.Window200) then failwith "osu!mania ln Window200 must be non-negative"
+                    if negative(windows.Window100) then failwith "osu!mania ln Window100 must be non-negative"
+                    if negative(windows.Window50) then failwith "osu!mania ln Window50 must be non-negative"
+                    if negative(windows.WindowOverhold200) then failwith "osu!mania ln WindowOverhold200 must be non-negative"
+                    if negative(windows.WindowOverhold100) then failwith "osu!mania ln WindowOverhold100 must be non-negative"
+
+                    // todo: make sure they are ordered in some way
+
+                | HeadTailCombineRule.HeadJudgementOr (early_window, late_window, judgement_if_dropped, judgement_if_overheld) ->
+
+                    if positive(early_window) then failwith "HeadJudgementOr `early_window` must be non-positive"
+                    if negative(late_window) then failwith "HeadJudgementOr `late_window` must be non-negative"
+                    if judgement_if_dropped >= ruleset.Judgements.Length then failwith "HeadJudgementOr `judgement_if_dropped` must be a valid judgement"
+                    if judgement_if_overheld >= ruleset.Judgements.Length then failwith "HeadJudgementOr `judgement_if_dropped` must be a valid judgement"
+
+            | HoldMechanics.OnlyRequireHold window ->
+                if window < 0.0f<ms / rate> then failwith "OnlyRequireHold window must be non-negative"
+            | HoldMechanics.JudgeReleasesSeparately (judgement_windows, judgement_if_dropped) ->
+                let mutable w_min = 0.0f<ms / rate>
+                let mutable w_max = 0.0f<ms / rate>
+                for w in judgement_windows do
+                    match w with
+                    | Some (early, late) ->
+                        if early > w_min then failwithf "Early release window %.3fms must be %.3fms or earlier" early w_min
+                        w_min <- early
+                        if late < w_max then failwithf "Late release window %.3fms must be %.3fms or late" late w_max
+                        w_max <- late
+                    | None -> ()
+                if judgement_if_dropped >= ruleset.Judgements.Length then failwith "JudgeReleasesSeparately `judgement_if_dropped` must be a valid judgement"
+            | HoldMechanics.OnlyJudgeReleases judgement_if_dropped ->
+                if judgement_if_dropped >= ruleset.Judgements.Length then failwith "OnlyJudgeReleases `judgement_if_dropped` must be a valid judgement"
+
+            match ruleset.Accuracy with
+            | AccuracyPoints.WifeCurve judge -> if judge < 2 || judge > 9 then failwith "WifeCurve `judge` must be a valid judge value"
+            | AccuracyPoints.PointsPerJudgement points ->
+                for p in points do
+                    if not (Double.IsFinite p) then failwith "PointsPerJudgement `points` contains invalid floating point values"
+
+            Ok ruleset
+        with err ->
+            Error err.Message
---
+++ prelude/tests/Rulesets/Validation.fs
@@ -0,0 +1,173 @@
+﻿namespace Prelude.Tests.Scoring
+
+open NUnit.Framework
+open Prelude
+open Prelude.Gameplay.RulesetsV2
+
+module Validation =
+
+    [<Test>]
+    let SCJ4_Valid () =
+        let ruleset = SC.create 4
+
+        match RulesetV2.check ruleset with
+        | Ok _ -> Assert.Pass()
+        | Error reason -> Assert.Fail(reason)
+
+    [<Test>]
+    let SC_AllJudges_Valid () =
+
+        for judge = 2 to 9 do
+            let ruleset = SC.create judge
+
+            match RulesetV2.check ruleset with
+            | Ok _ -> printfn "Judge %i valid" judge
+            | Error reason -> Assert.Fail(reason)
+
+        Assert.Pass()
+
+    let VALID_RULESET = SC.create 4
+
+    [<Test>]
+    let Judgements_Required () =
+        
+        let invalid_ruleset = { VALID_RULESET with Judgements = [||] }
+        
+        match RulesetV2.check invalid_ruleset with
+        | Ok _ -> Assert.Fail()
+        | Error reason -> Assert.Pass(reason)
+    
+    [<Test>]
+    let Judgements_WindowsSigns_EarlyWindow () =
+        
+        let invalid_ruleset = 
+            { VALID_RULESET with 
+                Judgements = 
+                    [|
+                        {
+                            Name = "A"
+                            Color = Color.White
+                            TimingWindows = Some (25.0f<ms / rate>, 50.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                        {
+                            Name = "B"
+                            Color = Color.White
+                            TimingWindows = Some (50.0f<ms / rate>, 100.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                    |]
+                HoldMechanics = HoldMechanics.OnlyJudgeReleases 0
+            }
+        
+        match RulesetV2.check invalid_ruleset with
+        | Ok _ -> Assert.Fail()
+        | Error reason -> Assert.Pass(reason)
+    
+    [<Test>]
+    let Judgements_WindowsSigns_LateWindow () =
+        
+        let invalid_ruleset = 
+            { VALID_RULESET with 
+                Judgements = 
+                    [|
+                        {
+                            Name = "A"
+                            Color = Color.White
+                            TimingWindows = Some (-50.0f<ms / rate>, -25.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                        {
+                            Name = "B"
+                            Color = Color.White
+                            TimingWindows = Some (-100.0f<ms / rate>, -50.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                    |]
+                HoldMechanics = HoldMechanics.OnlyJudgeReleases 0
+            }
+        
+        match RulesetV2.check invalid_ruleset with
+        | Ok _ -> Assert.Fail()
+        | Error reason -> Assert.Pass(reason)
+    
+    [<Test>]
+    let Judgements_WindowsOrdering () =
+        
+        let invalid_ruleset = 
+            { VALID_RULESET with 
+                Judgements = 
+                    [|
+                        {
+                            Name = "A"
+                            Color = Color.White
+                            TimingWindows = Some (-50.0f<ms / rate>, 50.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                        {
+                            Name = "B"
+                            Color = Color.White
+                            TimingWindows = Some (-25.0f<ms / rate>, -25.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                    |]
+                HoldMechanics = HoldMechanics.OnlyJudgeReleases 0
+            }
+        
+        match RulesetV2.check invalid_ruleset with
+        | Ok _ -> Assert.Fail()
+        | Error reason -> Assert.Pass(reason)
+    
+    [<Test>]
+    let Judgements_InvalidWindowValues_NaN () =
+        
+        let invalid_ruleset = 
+            { VALID_RULESET with 
+                Judgements = 
+                    [|
+                        {
+                            Name = "A"
+                            Color = Color.White
+                            TimingWindows = Some (nanf * 1.0f<ms / rate>, 50.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                        {
+                            Name = "B"
+                            Color = Color.White
+                            TimingWindows = Some (-100.0f<ms / rate>, 100.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                    |]
+                HoldMechanics = HoldMechanics.OnlyJudgeReleases 0
+            }
+        
+        match RulesetV2.check invalid_ruleset with
+        | Ok _ -> Assert.Fail()
+        | Error reason -> Assert.Pass(reason)
+
+    [<Test>]
+    let Judgements_InvalidWindowValues_Infinity () =
+        
+        let invalid_ruleset = 
+            { VALID_RULESET with 
+                Judgements = 
+                    [|
+                        {
+                            Name = "A"
+                            Color = Color.White
+                            TimingWindows = Some (-50.0f<ms / rate>, 50.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                        {
+                            Name = "B"
+                            Color = Color.White
+                            TimingWindows = Some (-infinityf * 1.0f<ms / rate>, 100.0f<ms / rate>)
+                            BreaksCombo = false
+                        }
+                    |]
+                HoldMechanics = HoldMechanics.OnlyJudgeReleases 0
+            }
+        
+        match RulesetV2.check invalid_ruleset with
+        | Ok _ -> Assert.Fail()
+        | Error reason -> Assert.Pass(reason)
