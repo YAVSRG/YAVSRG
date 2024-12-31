d78f29b7c7588e2d3b0eb75c9f59d3b29ebaf364
Percy Qaz <percyqaz@yahoo.co.uk>
2024-12-31 14:07:38 +0000

    👽️ Possible compatibility fix for GL contexts on windows

--- engine/src/Windowing/WindowThread.fs
+++ engine/src/Windowing/WindowThread.fs
@@ -334,8 +334,9 @@ module WindowThread =
         GLFW.WindowHint(WindowHintBool.Focused, false)
         GLFW.WindowHint(WindowHintBool.AutoIconify, true)
         GLFW.WindowHint(WindowHintBool.Decorated, false)
-        GLFW.WindowHint(WindowHintOpenGlProfile.OpenGlProfile, OpenGlProfile.Core)
-        GLFW.WindowHint(WindowHintBool.OpenGLForwardCompat, true)
+        if OperatingSystem.IsMacOS() then
+            GLFW.WindowHint(WindowHintOpenGlProfile.OpenGlProfile, OpenGlProfile.Core)
+            GLFW.WindowHint(WindowHintBool.OpenGLForwardCompat, true)
         GLFW.WindowHint(WindowHintInt.ContextVersionMajor, 3)
         GLFW.WindowHint(WindowHintInt.ContextVersionMinor, 3)
         GLFW.WindowHint(WindowHintInt.Samples, (if OperatingSystem.IsMacOS() then 0 else 24))
