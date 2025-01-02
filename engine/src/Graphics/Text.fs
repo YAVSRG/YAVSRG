12802e619fd9bf5225a325450323454b23ffd29a
Percy Qaz <percyqaz@yahoo.co.uk>
2025-01-02 15:51:30 +0000

    🌸 Improve some font 'twitching' and blurriness

--- engine/src/Graphics/Text.fs
+++ engine/src/Graphics/Text.fs
@@ -391,8 +391,8 @@ module Text =
     let draw_b (font: SpriteFont, text: string, scale, x, y, (fg: Drawing.Color, bg: Drawing.Color)) =
         let level, scale_mult =
             let l1 = scale / font.BaseScale
-            if l1 <= 1.0f then 0, l1
-            elif l1 <= 2.0f then 1, l1 / 2.0f
+            if l1 < 0.95f then 0, l1
+            elif l1 < 1.9f then 1, l1 / 2.0f
             else 2, l1 / 4.0f
         let char_lookup = font.CharLookup level
         let shadow_spacing = font.ShadowDepth * scale
