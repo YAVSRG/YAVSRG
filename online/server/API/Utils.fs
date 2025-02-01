2b11dc6c7eab39ca73481fc6734b7c3df18585b3
Percy Qaz <percyqaz@yahoo.co.uk>
2025-02-01 01:45:22 +0000

    👽️ Fix possible error caused by malforming an auth header

--- online/server/API/Utils.fs
+++ online/server/API/Utils.fs
@@ -20,11 +20,16 @@ module Utils =
     exception PermissionDeniedException
     exception BadRequestException of Message: string option
 
+    let BEARER_LENGTH = "Bearer ".Length
+
     let authorize (header: Map<string, string>) =
         if header.ContainsKey("Authorization") then
-            match User.by_auth_token (header.["Authorization"].Substring("Bearer ".Length)) with
-            | Some(id, user) -> id, user
-            | None -> raise AuthorizeFailedException
+            let auth_header = header.["Authorization"]
+            if auth_header.Length > BEARER_LENGTH then
+                match User.by_auth_token (header.["Authorization"].Substring(BEARER_LENGTH)) with
+                | Some(id, user) -> id, user
+                | None -> raise AuthorizeFailedException
+            else raise AuthorizeFailedException
         else
             raise NotAuthorizedException
 
