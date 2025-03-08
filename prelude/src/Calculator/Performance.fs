fff89fd083adecd1a42907d773da676fb25428d6
Percy Qaz <percyqaz@yahoo.co.uk>
2025-03-08 01:36:21 +0000

    🔥 Performance balance adjustments, comments

--- prelude/src/Calculator/Performance.fs
+++ prelude/src/Calculator/Performance.fs
@@ -1,9 +1,10 @@
 ﻿namespace Prelude.Calculator
 
-open System
 open Prelude.Gameplay.Scoring
 
 module Performance =
+
+    let ACC_SENSITIVITY = 0.97
 
     let acc_timeline (rr: Difficulty) (scoring: ScoreProcessor) =
         let mutable v = 1.0
@@ -16,18 +17,21 @@ module Performance =
                 i <- i + 1
             match ev.Action.Judgement with
             | Some (_, value) ->
-                v <- 0.95 * v + 0.05 * value
+                v <- ACC_SENSITIVITY * v + (1.0 - ACC_SENSITIVITY) * value
             | None -> ()
         output.[output.Length - 1] <- float32 v
         output
 
+    /// Some curves put together quickly in a day to be a 'good enough' proof of concept
+    /// Documentation coming soon, or if it didn't, bother me on discord for additional comment
+
     let tech_curve (accuracy: float32) =
         let x = 20.0f * (accuracy - 1.0f)
         let y = x / sqrt (1.0f + x * x)
-        0.85f * (1.0f + y) - 0.05f |> max 0.0f
+        0.65f * (1.0f + y) - 0.05f |> max 0.0f
 
     let physical_curve (accuracy: float32) =
-        let x = 9.25f - 10.0f * accuracy
+        let x = 9.15f - 10.0f * accuracy
         let y = x / sqrt (1.0f + x * x)
         0.5f - y
 
@@ -44,6 +48,9 @@ module Performance =
             SR = note.SR * note_mult
         }
 
+    /// Performance rating concept: Look at your accuracy throughout a score
+    /// Take the accuracy and scale notes up/down in the areas you are doing good/bad in
+    /// Now feed the new scaled notes through the same SR algorithm to get PR
     let calculate (rr: Difficulty) (scoring: ScoreProcessor) =
 
         let timeline = acc_timeline rr scoring
