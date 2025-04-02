bf5c0cb53eeb1bbb55a11a24e1f0ac31f4b1b5fb
Percy Qaz <percyqaz@yahoo.co.uk>
2025-04-02 21:13:38 +0100

    ⚙️ Start of an experimental LN coverage calculation

---
+++ prelude/src/Calculator/HoldCoverage.fs
@@ -0,0 +1,57 @@
+﻿namespace Prelude.Calculator
+
+open Prelude
+open Prelude.Charts
+
+module HoldCoverage =
+
+    let COVERAGE_WINDOW = 250.0f<ms / rate>
+
+    let calculate_coverage (keys: int, notes: TimeArray<NoteRow>, rate: Rate) : float32 array =
+
+        let output = Array.zeroCreate notes.Length
+        let holds_in_view = Array.zeroCreate keys
+        let window = COVERAGE_WINDOW * rate
+
+        let mutable front_index = -1
+        let mutable back_index = 0
+
+        let mutable start = 0.0f<ms>
+        let mutable in_hold = false
+
+        for i = 0 to notes.Length - 1 do
+            let time = notes.[i].Time
+
+            while front_index + 1 < notes.Length && notes.[front_index + 1].Time < time + window do
+                front_index <- front_index + 1
+                for k = 0 to keys - 1 do
+                    if notes.[front_index].Data.[k] = NoteType.HOLDHEAD then
+                        holds_in_view.[k] <- holds_in_view.[k] + 1
+
+            while back_index + 1 < notes.Length && notes.[back_index].Time < time - window do
+                for k = 0 to keys - 1 do
+                    if notes.[back_index].Data.[k] = NoteType.HOLDTAIL then
+                        holds_in_view.[k] <- holds_in_view.[k] - 1
+                        assert(holds_in_view.[k] >= 0)
+                back_index <- back_index + 1
+
+            for k = 0 to keys - 1 do
+                start <- time - window
+                in_hold <- false
+                for j = back_index to front_index do
+                    if notes.[j].Data.[k] = NoteType.HOLDHEAD then
+                        start <- max start notes.[j].Time
+                        in_hold <- true
+                    elif notes.[j].Data.[k] = NoteType.HOLDBODY then
+                        output.[i] <- output.[i] + float32 (notes.[j].Time - start)
+                        start <- max start notes.[j].Time
+                        in_hold <- true
+                    elif notes.[j].Data.[k] = NoteType.HOLDTAIL then
+                        output.[i] <- output.[i] + float32 (notes.[j].Time - start)
+                        in_hold <- false
+                if in_hold then
+                    output.[i] <- output.[i] + float32 (time + window - start)
+
+            output.[i] <- output.[i] / 2.0f / float32 window / float32 keys
+
+        output
---
+++ prelude/tests/Calculator/HoldCoverage.fs
@@ -0,0 +1,49 @@
+﻿namespace Prelude.Tests.Calculator
+
+open NUnit.Framework
+open Prelude
+open Prelude.Charts
+open Prelude.Calculator
+
+module HoldCoverage =
+
+    [<Test>]
+    let NotesHaveNoHolds() =
+
+        let notes =
+            [|
+                { Time = 0.0f<ms>; Data = [| NoteType.NORMAL; NoteType.NOTHING |]}
+                { Time = 100.0f<ms>; Data = [| NoteType.NOTHING; NoteType.NORMAL |]}
+            |]
+
+        let output = HoldCoverage.calculate_coverage (2, notes, 1.0f<rate>)
+
+        Assert.AreEqual([| 0.0f; 0.0f |], output)
+
+    [<Test>]
+    let NotesHaveOneHold() =
+
+        let notes =
+            [|
+                { Time = 0.0f<ms>; Data = [| NoteType.HOLDHEAD; NoteType.NOTHING |]}
+                { Time = 200.0f<ms>; Data = [| NoteType.HOLDTAIL; NoteType.NORMAL |]}
+            |]
+
+        let output = HoldCoverage.calculate_coverage (2, notes, 1.0f<rate>)
+
+        Assert.AreEqual([| 0.2f; 0.2f |], output)
+
+    [<Test>]
+    let NotesDuringBody() =
+
+        let notes =
+            [|
+                { Time = 0.0f<ms>; Data = [| NoteType.HOLDHEAD; NoteType.NOTHING |]}
+                { Time = 200.0f<ms>; Data = [| NoteType.HOLDBODY; NoteType.NORMAL |]}
+                { Time = 300.0f<ms>; Data = [| NoteType.HOLDBODY; NoteType.NORMAL |]}
+                { Time = 1000.0f<ms>; Data = [| NoteType.HOLDTAIL; NoteType.NOTHING |]}
+            |]
+
+        let output = HoldCoverage.calculate_coverage (2, notes, 1.0f<rate>)
+
+        Assert.AreEqual([| 0.25f; 0.45f; 0.5f; 0.25f |], output)
