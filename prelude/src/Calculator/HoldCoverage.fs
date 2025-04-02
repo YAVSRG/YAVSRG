d5f112c2cb092e7187dbd1bbde0b4129c9f98741
Percy Qaz <percyqaz@yahoo.co.uk>
2025-04-02 21:28:49 +0100

    ⚙️ Treat LNs as slightly shorter for coverage calculation

--- prelude/src/Calculator/HoldCoverage.fs
+++ prelude/src/Calculator/HoldCoverage.fs
@@ -6,12 +6,14 @@ open Prelude.Charts
 module HoldCoverage =
 
     let COVERAGE_WINDOW = 250.0f<ms / rate>
+    let SHORTEN_AMOUNT = 80.0f<ms / rate>
 
     let calculate_coverage (keys: int, notes: TimeArray<NoteRow>, rate: Rate) : float32 array =
 
         let output = Array.zeroCreate notes.Length
         let holds_in_view = Array.zeroCreate keys
         let window = COVERAGE_WINDOW * rate
+        let shorten = SHORTEN_AMOUNT * rate
 
         let mutable front_index = -1
         let mutable back_index = 0
@@ -22,7 +24,7 @@ module HoldCoverage =
         for i = 0 to notes.Length - 1 do
             let time = notes.[i].Time
 
-            while front_index + 1 < notes.Length && notes.[front_index + 1].Time < time + window do
+            while front_index + 1 < notes.Length && notes.[front_index + 1].Time < time + window + shorten do
                 front_index <- front_index + 1
                 for k = 0 to keys - 1 do
                     if notes.[front_index].Data.[k] = NoteType.HOLDHEAD then
@@ -40,14 +42,14 @@ module HoldCoverage =
                 in_hold <- false
                 for j = back_index to front_index do
                     if notes.[j].Data.[k] = NoteType.HOLDHEAD then
-                        start <- max start notes.[j].Time
+                        start <- max start notes.[j].Time |> min (time + window)
                         in_hold <- true
                     elif notes.[j].Data.[k] = NoteType.HOLDBODY then
-                        output.[i] <- output.[i] + float32 (notes.[j].Time - start)
-                        start <- max start notes.[j].Time
+                        output.[i] <- output.[i] + float32 (min (time + window) notes.[j].Time - start)
+                        start <- max start notes.[j].Time |> min (time + window)
                         in_hold <- true
                     elif notes.[j].Data.[k] = NoteType.HOLDTAIL then
-                        output.[i] <- output.[i] + float32 (notes.[j].Time - start)
+                        output.[i] <- output.[i] + float32 (notes.[j].Time - shorten - start)
                         in_hold <- false
                 if in_hold then
                     output.[i] <- output.[i] + float32 (time + window - start)
--- prelude/tests/Calculator/HoldCoverage.fs
+++ prelude/tests/Calculator/HoldCoverage.fs
@@ -31,7 +31,9 @@ module HoldCoverage =
 
         let output = HoldCoverage.calculate_coverage (2, notes, 1.0f<rate>)
 
-        Assert.AreEqual([| 0.2f; 0.2f |], output)
+        let expected = (200.0f<ms> - HoldCoverage.SHORTEN_AMOUNT * 1.0f<rate>) / 1000.0f<ms>
+
+        Assert.AreEqual([| expected;expected |], output)
 
     [<Test>]
     let NotesDuringBody() =
@@ -41,9 +43,25 @@ module HoldCoverage =
                 { Time = 0.0f<ms>; Data = [| NoteType.HOLDHEAD; NoteType.NOTHING |]}
                 { Time = 200.0f<ms>; Data = [| NoteType.HOLDBODY; NoteType.NORMAL |]}
                 { Time = 300.0f<ms>; Data = [| NoteType.HOLDBODY; NoteType.NORMAL |]}
-                { Time = 1000.0f<ms>; Data = [| NoteType.HOLDTAIL; NoteType.NOTHING |]}
+                { Time = 1000.0f<ms>; Data = [| NoteType.HOLDBODY; NoteType.NORMAL |]}
+                { Time = 1000.0f<ms> + HoldCoverage.SHORTEN_AMOUNT * 1.0f<rate>; Data = [| NoteType.HOLDTAIL; NoteType.NOTHING |]}
             |]
 
         let output = HoldCoverage.calculate_coverage (2, notes, 1.0f<rate>)
 
-        Assert.AreEqual([| 0.25f; 0.45f; 0.5f; 0.25f |], output)
+        Assert.AreEqual([| 0.25f; 0.45f; 0.5f; 0.25f; 0.17f |], output)
+
+    [<Test>]
+    let TwoHoldNotes() =
+
+        let notes =
+            [|
+                { Time = 0.0f<ms>; Data = [| NoteType.HOLDHEAD; NoteType.NOTHING |]}
+                { Time = 300.0f<ms>; Data = [| NoteType.HOLDBODY; NoteType.HOLDHEAD |]}
+                { Time = 600.0f<ms>; Data = [| NoteType.HOLDTAIL; NoteType.HOLDBODY |]}
+                { Time = 900.0f<ms>; Data = [| NoteType.NOTHING; NoteType.HOLDTAIL |]}
+            |]
+
+        let output = HoldCoverage.calculate_coverage (2, notes, 1.0f<rate>)
+
+        Assert.AreEqual([| 0.25f; 0.72f; 0.64f; 0.17f |], output)
