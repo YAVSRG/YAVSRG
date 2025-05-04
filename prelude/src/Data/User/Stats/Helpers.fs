a55f4b1cde1bf5962577ac26a28e7bd51797d39d
Percy Qaz <percyqaz@yahoo.co.uk>
2025-05-04 22:15:44 +0100

    🌸 Format total playtime in hours instead of days

--- prelude/src/Data/User/Stats/Helpers.fs
+++ prelude/src/Data/User/Stats/Helpers.fs
@@ -32,11 +32,8 @@ module StatsHelpers =
         let seconds = time / 1000.0
         let minutes = seconds / 60.0
         let hours = minutes / 60.0
-        let days = hours / 24.0
 
-        if days > 1 then
-            sprintf "%id %02ih %02im" (floor days |> int) (floor (hours % 24.0) |> int) (floor (minutes % 60.0) |> int)
-        elif hours > 1 then
+        if hours > 1 then
             sprintf "%ih %02im" (floor hours |> int) (floor (minutes % 60.0) |> int)
         else
             sprintf "%im %02is" (floor minutes |> int) (floor (seconds % 60.0) |> int)
