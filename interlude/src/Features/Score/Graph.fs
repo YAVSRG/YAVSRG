0defeacdcd734fe663aaf35e126676bb460e2417
Muffletruffle <147563143+Muffletruffle@users.noreply.github.com>
2026-07-12 11:57:10 -0600

    refactor: minor formatting

--- interlude/src/Features/Score/Graph.fs
+++ interlude/src/Features/Score/Graph.fs
@@ -175,12 +175,12 @@ and ScoreGraph(score_info: ScoreInfo, stats: ScoreScreenStats ref) =
         let text_color = if stats.Value.ColumnFilterApplied then Colors.text_green else Colors.text
         let judgement_count = Array.sum info.Judgements
 
-        let ma = if info.Judgements.Length <= 1 then sprintf "  •  MA: %i.0:0" info.Judgements[0]
-                    else sprintf "  •  MA: %.1f:%i" (float32 info.Judgements.[0] / float32  (max 1 info.Judgements.[1])) (if info.Judgements.[1] < 1 then 0 else 1)
+        let ma = if info.Judgements.Length <= 1 then sprintf "  •  MA: %i.0:0" info.Judgements.[0]
+                    else sprintf "  •  MA: %.1f:%i" (float32 info.Judgements.[0] / float32  (max 1 info.Judgements.[1])) (min info.Judgements.[1] 1)
 
         let pa = if info.Judgements.Length <= 1 then sprintf "  •  PA: 0.0:0"
-                    else if info.Judgements.Length <= 2 then sprintf "  •  PA: %i.0:0" info.Judgements[1]
-                    else sprintf "  •  PA: %.1f:%i" (float32 info.Judgements.[1] / float32 (max 1 info.Judgements.[2])) (if info.Judgements.[2] < 1 then 0 else 1)
+                    else if info.Judgements.Length <= 2 then sprintf "  •  PA: %i.0:0" info.Judgements.[1]
+                    else sprintf "  •  PA: %.1f:%i" (float32 info.Judgements.[1] / float32 (max 1 info.Judgements.[2])) (min info.Judgements.[2] 1)
 
         Text.fill_b (
             Style.font,
