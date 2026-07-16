db9a213f6d43522afeb3707a923e12e417691f38
Percy Qaz <percyqaz@yahoo.co.uk>
2026-07-16 21:03:58 +0100

    fix: rate meter not updating when rate changed in practice mode

--- interlude/src/Features/Skins/Elements/RateMods.fs
+++ interlude/src/Features/Skins/Elements/RateMods.fs
@@ -4,19 +4,23 @@ open Percyqaz.Flux.UI
 open Prelude.Mods
 open Prelude.Skins.HudLayouts
 open Interlude.Features.Play
-open Interlude.Features.Gameplay
 
 type RateMods(ctx: HudContext) =
     inherit Container(NodeType.None)
 
-    override this.Init(parent) =
-        let text =
+    override this.Init(parent: Widget) : unit =
+        let update_text() : string =
             if ctx.Config.RateModMeterShowMods then
-                ModState.format (SelectedChart.rate.Value, ctx.State.WithColors.ModsSelected)
+                ModState.format (ctx.State.Scoring.Rate, ctx.State.WithColors.ModsSelected)
             else
-                sprintf "%.2fx" SelectedChart.rate.Value
+                sprintf "%.2fx" ctx.State.Scoring.Rate
+                
+        let mutable text = update_text()
+        ctx.State.OnScoringChanged(fun () -> text <- update_text()) |> ignore
 
-        this |* Text(text)
-            .Color(Colors.text_subheading)
-            .Align(Alignment.CENTER)
-        base.Init parent
+        this.Add(
+            Text(fun () -> text)
+                .Color(Colors.text_subheading)
+                .Align(Alignment.CENTER)
+        )
+        base.Init(parent)
