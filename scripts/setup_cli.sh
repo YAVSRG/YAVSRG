f92d65165fbda53b8e7d7ea667c6fbb1ee31417c
sevs55 <none>
2026-07-27 22:46:30 +0200

    chore: improve CLI setup script

--- scripts/setup_cli.sh
+++ scripts/setup_cli.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 cd ../tools
-dotnet tool uninstall -g YAVSRG.CLI
+dotnet tool uninstall -g YAVSRG.CLI || true
 dotnet pack
 dotnet tool install -g --add-source ./nupkg YAVSRG.CLI
-cd ../scripts
