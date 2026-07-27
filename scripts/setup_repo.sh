7bcaa82bfb1569a981deb53b5d281181a17e12f3
sevs55 <none>
2026-07-27 22:46:30 +0200

    chore: improve repository setup script

--- scripts/setup_repo.sh
+++ scripts/setup_repo.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 cd ..
-git submodule update --init libraries/Percyqaz.Common
-git submodule update --init libraries/Percyqaz.Data
-git submodule update --init libraries/Percyqaz.Shell
-cd interlude
-cd ../scripts
+git submodule update --init \
+    libraries/Percyqaz.Common \
+    libraries/Percyqaz.Data \
+    libraries/Percyqaz.Shell
